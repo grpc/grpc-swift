515bfdf822ae6e7e19577a57c110d01694670879
Tim Burks <timburks@google.com>
2016-08-30 21:58:50 +0000

    Update Packages Makefile for QuickProto + disable one unsupportable API

--- Packages/QuickProto/Sources/FileDescriptorSet.swift
+++ Packages/QuickProto/Sources/FileDescriptorSet.swift
@@ -51,12 +51,14 @@ public class FileDescriptorSet {
     }
   }
 
+#if !SWIFT_PACKAGE
   convenience public init(filename:String) {
     let path = Bundle.main.resourcePath!.appending("/").appending(filename)
     let fileDescriptorSetProto = NSData(contentsOfFile:path)
     assert(fileDescriptorSetProto != nil) // the file to be loaded must be in the resource bundle
     self.init(proto:fileDescriptorSetProto!)
   }
+#endif
 
   init(message:Message) {
     message.forEachField(path:["file"]) { (field) in
