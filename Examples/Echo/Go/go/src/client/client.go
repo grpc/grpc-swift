03243903e872d95b5224e9e70110e35d793c02b9
Tim Burks <timburks@google.com>
2017-01-07 22:09:11 -0800

    Remove unnecessary channel from Go expand client.

--- Examples/Echo/Go/go/src/client/client.go
+++ Examples/Echo/Go/go/src/client/client.go
@@ -144,12 +144,10 @@ func call_expand(c pb.EchoClient, message string) {
 	if err != nil {
 		panic(err)
 	}
-	waitc := make(chan struct{})
 	for {
 		in, err := stream.Recv()
 		if err == io.EOF {
 			// read done.
-			close(waitc)
 			return
 		}
 		if err != nil {
@@ -157,5 +155,4 @@ func call_expand(c pb.EchoClient, message string) {
 		}
 		log.Printf("Received: %s", in.Text)
 	}
-	<-waitc
 }
