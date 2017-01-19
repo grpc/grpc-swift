220cc3fe2947c384895cdf752aa6828ab5c6f713
Tim Burks <timburks@google.com>
2017-01-19 09:23:23 -0800

    Update vendoring scripts and build configuration for examples for new vendored gRPC Core.

---
+++ Sources/BoringSSL/README.md
@@ -0,0 +1,4 @@
+# BoringSSL source files
+
+This directory contains vendored code for BoringSSL.
+
--- vendor-grpc.sh
+++ vendor-grpc.sh
@@ -1,4 +1,38 @@
 #!/bin/sh
+#
+# Copyright 2016, Google Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+#     * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above
+# copyright notice, this list of conditions and the following disclaimer
+# in the documentation and/or other materials provided with the
+# distribution.
+#     * Neither the name of Google Inc. nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# This script vendors the gRPC Core library into the
+# CgRPC module in a form suitable for building with
+# the Swift Package Manager.
+#
 
 public_headers=(
 'include/grpc/support/alloc.h'
@@ -712,17 +746,23 @@ private_headers=(
 'src/core/ext/census/rpc_metric_id.h'
 'src/core/ext/census/trace_context.h')
 
+rm -rf Sources/CgRPC/src
+rm -rf Sources/CgRPC/third_party
+rm -rf Sources/CgRPC/grpc
+
 for src in "${public_headers[@]}"
 do
-	dest="Sources/gRPC_Core/$src"
+	dest="Sources/CgRPC/$src"
 	dest_dir=$(dirname $dest)
 	mkdir -p $dest_dir
 	cp third_party/grpc/$src $dest
 done
 
+mv Sources/CgRPC/include/grpc Sources/CgRPC/grpc
+
 for src in "${source_files[@]}"
 do
-	dest="Sources/gRPC_Core/$src"
+	dest="Sources/CgRPC/$src"
 	dest_dir=$(dirname $dest)
 	mkdir -p $dest_dir
 	cp third_party/grpc/$src $dest
@@ -730,7 +770,7 @@ done
 
 for src in "${private_headers[@]}"
 do
-	dest="Sources/gRPC_Core/$src"
+	dest="Sources/CgRPC/$src"
 	dest_dir=$(dirname $dest)
 	mkdir -p $dest_dir
 	cp third_party/grpc/$src $dest
