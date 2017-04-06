0c1c3bc7b8605ccf2e947ecc229e13049802294a
John Pope <jp@bellgeorge.com>
2017-04-06 10:48:40 -0400

    update runme.sh

--- third_party/RUNME.sh
+++ third_party/RUNME.sh
@@ -2,14 +2,26 @@
 printf "\033c"
 read -p "Would you like to download and install swift-protobuf / grpc [y/N]" CONDITION;
 if [ "$CONDITION" == "y" ] ; then
-    git clone https://github.com/apple/swift-protobuf.git
-    cd swift-protobuf
-    make install
-    cd ..
-    git clone https://github.com/grpc/grpc.git
-    cd grpc
-    git submodule update --init
-    make install
+    read -p "Use last known stable branch - 0.9.24 ? or  latest master ? [S/l]  " CONDITION;
+    if [ "$CONDITION" == "l" ] ; then
+        git clone https://github.com/apple/swift-protobuf.git
+        cd swift-protobuf
+        make install
+        cd ..
+        git clone https://github.com/grpc/grpc.git
+        cd grpc
+        git submodule update --init
+        make install
+    else
+        git clone -b 0.9.24 https://github.com/apple/swift-protobuf.git
+        cd swift-protobuf
+        make install
+        cd ..
+        git clone https://github.com/grpc/grpc.git
+        cd grpc
+        git submodule update --init
+        make install
+    fi
 fi
 
 
