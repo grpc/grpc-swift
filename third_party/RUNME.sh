3b04257c9f394c2fae181163d641f1c84c0b78af
John Pope <jp@bellgeorge.com>
2017-04-06 10:40:44 -0400

    Update RUNME.sh
    spoon feeding.

--- third_party/RUNME.sh
+++ third_party/RUNME.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
+printf "\033c"
+read -p "Would you like to download and install swift-protobuf / grpc [y/N]" CONDITION;
+if [ "$CONDITION" == "y" ] ; then
+    git clone https://github.com/apple/swift-protobuf.git
+    cd swift-protobuf
+    make install
+    cd ..
+    git clone https://github.com/grpc/grpc.git
+    cd grpc
+    git submodule update --init
+    make install
+fi
 
-git clone -b 0.9.24 https://github.com/apple/swift-protobuf.git
-git clone https://github.com/grpc/grpc.git
 
