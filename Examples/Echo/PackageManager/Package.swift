e69f5266e0f4a1808a9cd1e10458847b783c7469
Cyrille Hemidy <cyrille.hemidy@gmail.com>
2017-09-01 14:13:01 +0200

    Update swift-protobuf dependency version to 0.9.904

--- Examples/Echo/PackageManager/Package.swift
+++ Examples/Echo/PackageManager/Package.swift
@@ -18,6 +18,6 @@ let package = Package (
     name: "Echo",
     dependencies: [
         .Package(url: "https://github.com/grpc/grpc-swift.git", Version(0,1,12)),
-        .Package(url: "https://github.com/apple/swift-protobuf.git", Version(0,9,903)),
+        .Package(url: "https://github.com/apple/swift-protobuf.git", Version(0,9,904)),
     ]
 )
