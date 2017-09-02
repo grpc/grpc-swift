bc1f6acbb52cd96c0602fb6d7a729881ba3d6d3e
Tim Burks <timburks@google.com>
2017-09-01 20:30:30 -0700

    Update package dependencies for samples.

--- Examples/Datastore/PackageManager/Package.swift
+++ Examples/Datastore/PackageManager/Package.swift
@@ -17,8 +17,8 @@ import PackageDescription
 let package = Package (
     name: "Datastore",
     dependencies: [
-        .Package(url: "https://github.com/grpc/grpc-swift.git", Version(0,1,13)),
-        .Package(url: "https://github.com/apple/swift-protobuf.git", Version(0,9,903)),
+        .Package(url: "https://github.com/grpc/grpc-swift.git", Version(0,2,0)),
+        .Package(url: "https://github.com/apple/swift-protobuf.git", Version(0,9,904)),
         .Package(url: "https://github.com/google/auth-library-swift.git", Version(0,2,2)),
     ]
 )
