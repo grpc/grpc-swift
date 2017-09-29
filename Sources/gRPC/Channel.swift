9300fe5bda3ba6e017875e9a827b49236fca53dd
ujjwalap <26560455+ujjwalap@users.noreply.github.com>
2017-09-29 10:41:57 -0700

    Update Channel.swift

--- Sources/gRPC/Channel.swift
+++ Sources/gRPC/Channel.swift
@@ -67,7 +67,7 @@ public class Channel {
   public init(address:String,certificates: String?, privateKey: String?, certChain: String?){
         
         self.host = address
-        underlyingChannel = cgrpc_channel_create_secure_asm(address, certificates, privateKey, certChain )
+        underlyingChannel = cgrpc_channel_create_securev2(address, certificates, privateKey, certChain )
         completionQueue = CompletionQueue(underlyingCompletionQueue:cgrpc_channel_completion_queue(underlyingChannel))
         completionQueue.name = "Client" // only for debugging
         self.completionQueue.run() // start a loop that watches the channel's completion queue
