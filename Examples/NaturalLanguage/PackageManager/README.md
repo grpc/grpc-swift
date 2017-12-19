ba6eb58aeb699957e13290fe333fb294fa8b6bf8
Tim Burks <timburks@google.com>
2017-12-19 10:12:06 -0800

    Add sample calling the Google Cloud Natural Language API.
    This sample uses service-account-based authentication supported
    by google/auth-library-swift.

---
+++ Examples/NaturalLanguage/PackageManager/README.md
@@ -0,0 +1,24 @@
+# Calling the Google Cloud Natural Language API
+
+This directory contains a very simple sample that calls the 
+[Google Cloud Natural Language API](https://cloud.google.com/datastore/docs/reference/rpc/google.datastore.v1).
+Calls are made directly to the Cloud Natural Language RPC interface. 
+In practice, these would be wrapped in idiomatic code.
+
+Use [RUNME](RUNME) to generate the necessary Protocol Buffer
+and gRPC support code. It uses protoc and the Swift Protocol
+Buffer and gRPC plugins, so please be sure these are in your
+path. The plugins can be built by running `make` in the 
+top-level Plugins directory.
+
+Calls require a Google project ID and service account credentials.
+
+To create a project ID, visit the 
+[Google Cloud Console](https://cloud.google.com/console).
+
+After enabling the Cloud Natural Language API for your project,
+create and download service account credentials, saving them
+in ~/.credentials/credentials.json.
+
+Service account support is provided by Google's 
+[Auth Library for Swift](https://github.com/google/auth-library-swift).
---
+++ Examples/NaturalLanguage/PackageManager/Sources/main.swift
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2017, gRPC Authors All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import Foundation
+import gRPC
+import OAuth2
+
+let CREDENTIALS = ".credentials/credentials.json"
+
+if #available(OSX 10.12, *) {
+  let homeURL = FileManager.default.homeDirectoryForCurrentUser
+  let credentialsURL = homeURL.appendingPathComponent(CREDENTIALS)
+  if let provider = ServiceAccountTokenProvider(credentialsURL:credentialsURL) {
+    let sem = DispatchSemaphore(value: 0)
+    try provider.withToken() {(token, error) -> Void in
+      if let token = token {
+
+        gRPC.initialize()
+
+        guard let authToken = token.AccessToken else {
+          print("ERROR: No OAuth token is available.")
+          exit(-1)
+        }
+
+        let certificateURL = URL(fileURLWithPath:"roots.pem")
+        let certificates = try! String(contentsOf: certificateURL, encoding: .utf8)
+        let service = Google_Cloud_Language_V1_LanguageServiceService(address:"language.googleapis.com",
+                                                                      certificates:certificates,
+                                                                      host:nil)
+
+        service.metadata = Metadata(["authorization":"Bearer " + authToken])
+
+        var request = Google_Cloud_Language_V1_AnnotateTextRequest()
+
+        var document = Google_Cloud_Language_V1_Document()
+        document.type = .plainText
+        document.content = "The Caterpillar and Alice looked at each other for some time in silence: at last the Caterpillar took the hookah out of its mouth, and addressed her in a languid, sleepy voice. `Who are you?' said the Caterpillar."
+        request.document = document
+
+        var features = Google_Cloud_Language_V1_AnnotateTextRequest.Features()
+        features.extractSyntax = true
+        features.extractEntities = true
+        features.extractDocumentSentiment = true
+        features.extractEntitySentiment = true
+        features.classifyText = true
+        request.features = features
+
+        print("\(request)")
+
+        do {
+          let result = try service.annotatetext(request)
+          print("\(result)")
+        } catch (let error) {
+          print("ERROR: \(error)")
+        }
+
+      }
+      if let error = error {
+        print("ERROR \(error)")
+      }
+      sem.signal()
+    }
+    _ = sem.wait(timeout: DispatchTime.distantFuture)
+  } else {
+    print("Unable to read service account credentials from $HOME/\(CREDENTIALS).")
+  }
+} else {
+  print("This sample requires OSX 10.12 or later.")
+}
+
