c92da144e9da1645ce8412fc9b0c23c41be42214
Tim Burks <timburks@google.com>
2017-12-28 21:30:17 -0800

    Update Example dependencies to use 0.3.2

--- Examples/Simple/PackageManager/Package.swift
+++ Examples/Simple/PackageManager/Package.swift
@@ -17,6 +17,6 @@ import PackageDescription
 let package = Package (
     name: "Simple",
     dependencies: [
-        .Package(url: "https://github.com/grpc/grpc-swift.git", Version(0,3,0)),
+        .Package(url: "https://github.com/grpc/grpc-swift.git", Version(0,3,2)),
     ]
 )
