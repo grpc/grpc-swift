08309ecd3e5711f48a7ec83d3dd2b7adce61cb07
Tim Burks <timburks@google.com>
2017-12-28 21:15:47 -0800

    Update main Echo sample to use Commander.

--- Examples/Echo/PackageManager/Package.swift
+++ Examples/Echo/PackageManager/Package.swift
@@ -17,7 +17,8 @@ import PackageDescription
 let package = Package (
     name: "Echo",
     dependencies: [
-        .Package(url: "https://github.com/grpc/grpc-swift.git", Version(0,3,0)),
+        .Package(url: "https://github.com/grpc/grpc-swift.git", Version(0,3,1)),
         .Package(url: "https://github.com/apple/swift-protobuf.git", Version(1,0,2)),
+        .Package(url: "https://github.com/kylef/Commander.git", Version(0,8,0)),
     ]
 )
