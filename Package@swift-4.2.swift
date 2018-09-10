ee0c44227ee9acca3baac8c709e981bc369e4cc4
Keith Smiley <keithbsmiley@gmail.com>
2018-09-10 10:02:35 -0700

    Add Package.swift for Swift 4.2
    In order to allow dependent packages to build SwiftGRPC with Swift 4.2,
    we have to use the new Package.swift format to define
    `swiftLanguageVersions` that include the new enum values. In order to
    remain backwards compatible with Xcode < 10 and Swift compilers before
    4.2 on Linux we have to duplicate the Package.swift file and make one
    specifically for 4.2.

---
+++ Package@swift-4.2.swift
@@ -0,0 +1,65 @@
+// swift-tools-version:4.2
+/*
+ * Copyright 2017, gRPC Authors All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import PackageDescription
+
+var packageDependencies: [Package.Dependency] = [
+  .package(url: "https://github.com/apple/swift-protobuf.git", .upToNextMinor(from: "1.1.1")),
+  .package(url: "https://github.com/kylef/Commander.git", from: "0.8.0"),
+  .package(url: "https://github.com/apple/swift-nio-zlib-support.git", from: "1.0.0")
+]
+
+var cGRPCDependencies: [Target.Dependency] = []
+#if os(Linux)
+// On Linux, Foundation links with openssl, so we'll need to use that instead of BoringSSL.
+// See https://github.com/apple/swift-nio-ssl/issues/16#issuecomment-392705505 for details.
+packageDependencies.append(.package(url: "https://github.com/apple/swift-nio-ssl-support.git", from: "1.0.0"))
+#else
+cGRPCDependencies.append("BoringSSL")
+#endif
+
+let package = Package(
+  name: "SwiftGRPC",
+  products: [
+    .library(name: "SwiftGRPC", targets: ["SwiftGRPC"]),
+  ],
+  dependencies: packageDependencies,
+  targets: [
+    .target(name: "SwiftGRPC",
+            dependencies: ["CgRPC", "SwiftProtobuf"]),
+    .target(name: "CgRPC",
+            dependencies: cGRPCDependencies),
+    .target(name: "RootsEncoder"),
+    .target(name: "protoc-gen-swiftgrpc",
+            dependencies: [
+              "SwiftProtobuf",
+              "SwiftProtobufPluginLibrary",
+              "protoc-gen-swift"]),
+    .target(name: "BoringSSL"),
+    .target(name: "Echo",
+            dependencies: [
+              "SwiftGRPC",
+              "SwiftProtobuf",
+              "Commander"],
+            path: "Sources/Examples/Echo"),
+    .target(name: "Simple",
+            dependencies: ["SwiftGRPC", "Commander"],
+            path: "Sources/Examples/Simple"),
+    .testTarget(name: "SwiftGRPCTests", dependencies: ["SwiftGRPC"])
+  ],
+  swiftLanguageVersions: [.v4, .v4_2],
+  cLanguageStandard: .gnu11,
+  cxxLanguageStandard: .cxx11)
