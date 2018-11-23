a347855ba2d20befe1c2f146c2152820e9faa703
Daniel Alm <Daniel.Alm@ForumD.net>
2018-11-23 11:46:06 +0100

    Remove two unnecessary server channel options.

--- Sources/SwiftGRPCNIO/GRPCServer.swift
+++ Sources/SwiftGRPCNIO/GRPCServer.swift
@@ -37,8 +37,6 @@ public final class GRPCServer {
       // Enable TCP_NODELAY and SO_REUSEADDR for the accepted Channels
       .childChannelOption(ChannelOptions.socket(IPPROTO_TCP, TCP_NODELAY), value: 1)
       .childChannelOption(ChannelOptions.socket(SocketOptionLevel(SOL_SOCKET), SO_REUSEADDR), value: 1)
-      .childChannelOption(ChannelOptions.maxMessagesPerRead, value: 1)
-      .childChannelOption(ChannelOptions.allowRemoteHalfClosure, value: true)
 
     return bootstrap.bind(host: hostname, port: port)
       .map { GRPCServer(channel: $0) }
