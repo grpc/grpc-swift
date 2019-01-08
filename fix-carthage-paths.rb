cfb8873acb9d74e582e0869f2f1380997e6f23c8
Dmitry Malakhov <dmitry.malakhov@3drobotics.com>
2019-01-08 12:35:09 -0800

    Moved the fix-carthage-paths script  to pre-build actions

--- fix-carthage-paths.rb
+++ fix-carthage-paths.rb
@@ -8,41 +8,25 @@ if !dependenciesGroup.nil?
     puts dependenciesGroup.name
 end
 
-localDir = ENV["PWD"]
-
 dependenciesGroup.recursive_children_groups.each do |child|
     if !Dir.exists?(child.real_path)
         path = child.path
 
         stringArray = path.split(".build/checkouts/").last.split("/")
-        repoNameInProject = stringArray[0]
+        repoNameInXcodeproj = stringArray[0]
         
-        if !repoNameInProject.nil? and repoNameInProject.include? ".git-"
+        if !repoNameInXcodeproj.nil? and repoNameInXcodeproj.include? ".git-"
             
-            repoName = repoNameInProject.split(".git-").first
-            dirPath = Dir.glob("#{localDir}/.build/checkouts/#{repoName}**").first
-#            pathname = Pathname(dirPath)
+            repoName = repoNameInXcodeproj.split(".git-").first
+            
+            projectDir = ENV["PWD"]
+            spmDirPath = Dir.glob("#{projectDir}/.build/checkouts/#{repoName}**").first
+            xcodeprojDirPath = "#{projectDir}/.build/checkouts/#{repoNameInXcodeproj}"
 
-            newDirPath = "#{localDir}/.build/checkouts/#{repoNameInProject}"
-            p dirPath
-            p newDirPath
-            p repoNameInProject
-            if !dirPath.eql? newDirPath
-                p("rename #{dirPath} to #{newDirPath}")
-                FileUtils.mv dirPath, newDirPath
+            if !spmDirPath.eql? xcodeprojDirPath
+                # Rename directory created by SPM to the name that Xcodeproj file already had
+                FileUtils.mv spmDirPath, xcodeprojDirPath
             end
-            
-            
-#            newDirName = repoName + pathname.basename.to_s.split(repoName).last
-#
-#            if !repoNameInProject.nil?
-#                stringArray[0] = newDirName
-#                relativePath = ".build/checkouts/" + stringArray.join("/")
-#                p relativePath
-##                child.set_path(relativePath)
-#            end
         end
     end
 end
-#p("End of script")
-#project.save
