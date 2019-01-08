c13866b532b5ab3589b429ab364caa7c11067a2e
Dmitry Malakhov <dmitry.malakhov@3drobotics.com>
2019-01-08 11:43:27 -0800

    Carthage support WIP

--- fix-carthage-paths.rb
+++ fix-carthage-paths.rb
@@ -15,23 +15,34 @@ dependenciesGroup.recursive_children_groups.each do |child|
         path = child.path
 
         stringArray = path.split(".build/checkouts/").last.split("/")
-        string = stringArray[0]
+        repoNameInProject = stringArray[0]
         
-        if !string.nil? and string.include? ".git-"
-            repoName = string.split(".git-").first
+        if !repoNameInProject.nil? and repoNameInProject.include? ".git-"
+            
+            repoName = repoNameInProject.split(".git-").first
             dirPath = Dir.glob("#{localDir}/.build/checkouts/#{repoName}**").first
-            pathname = Pathname(dirPath)
-            newDirName = repoName + pathname.basename.to_s.split(repoName).last
+#            pathname = Pathname(dirPath)
 
-            if !string.nil?
-                stringArray[0] = newDirName
-                relativePath = ".build/checkouts/" + stringArray.join("/")
-                p relativePath
-                child.set_path(relativePath)
-
+            newDirPath = "#{localDir}/.build/checkouts/#{repoNameInProject}"
+            p dirPath
+            p newDirPath
+            p repoNameInProject
+            if !dirPath.eql? newDirPath
+                p("rename #{dirPath} to #{newDirPath}")
+                FileUtils.mv dirPath, newDirPath
             end
+            
+            
+#            newDirName = repoName + pathname.basename.to_s.split(repoName).last
+#
+#            if !repoNameInProject.nil?
+#                stringArray[0] = newDirName
+#                relativePath = ".build/checkouts/" + stringArray.join("/")
+#                p relativePath
+##                child.set_path(relativePath)
+#            end
         end
     end
 end
-
-project.save
+#p("End of script")
+#project.save
