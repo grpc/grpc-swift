1998dd1c4a03203ba0d72692b013106e4caf2f51
Dmitry Malakhov <dmitry.malakhov@3drobotics.com>
2019-01-09 14:23:10 -0800

    More fixes for Carthage

--- patch-carthage-project.rb
+++ patch-carthage-project.rb
@@ -2,7 +2,7 @@ require 'xcodeproj'
 project_path = ARGV[0]
 project = Xcodeproj::Project.open(project_path)
 
-# Remove targets that we do not want Carthage to build, and set the deployment target to 9.0.
+# 1) Remove targets that we do not want Carthage to build, and set the deployment target to 9.0.
 carthage_targets = ["BoringSSL", "CgRPC", "SwiftGRPC", "SwiftProtobuf"]
 targets_to_remove = project.targets.select { |target| !carthage_targets.include?(target.name) }
 targets_to_remove.each do |target|
@@ -10,11 +10,20 @@ targets_to_remove.each do |target|
 end
 project.save
 
-# Adding to SwiftGRPC-Package.xcscheme a script to Pre-Actions of BuildAction.
-# Script will resolve SPM dependecies and will fix paths issues for SwiftGRPC-Carthage.xcodeproj before everytime before build action
+# 2) Add SwiftProtobuf to the build actions list
 schemePath = Xcodeproj::XCScheme.shared_data_dir(project_path) + "SwiftGRPC-Package.xcscheme"
 scheme = Xcodeproj::XCScheme.new(schemePath)
 
+target = project.targets.select { |target| target.name == "SwiftProtobuf" }.first
+newBuildAction = Xcodeproj::XCScheme::BuildAction::Entry.new(target)
+newBuildAction.build_for_archiving = true
+newBuildAction.build_for_profiling = true
+newBuildAction.build_for_running = true
+newBuildAction.build_for_testing = true
+scheme.build_action.add_entry(newBuildAction)
+
+# 3) Adding to SwiftGRPC-Package.xcscheme a script to Pre-Actions of BuildAction.
+# Script will resolve SPM dependecies and will fix paths issues for SwiftGRPC-Carthage.xcodeproj before everytime before build action
 buildActions = scheme.build_action.xml_element
 
 preActions = REXML::Element.new("PreActions")
@@ -31,9 +40,9 @@ environmentBuildable = REXML::Element.new("EnvironmentBuildable", actionContent)
 
 buildableReference = REXML::Element.new("BuildableReference", environmentBuildable)
 buildableReference.add_attribute("BuildableIdentifier","primary")
-buildableReference.add_attribute("BlueprintIdentifier","SwiftGRPC::SwiftGRPC")
-buildableReference.add_attribute("BuildableName","SwiftGRPC.framework")
-buildableReference.add_attribute("BlueprintName","SwiftGRPC")
+buildableReference.add_attribute("BlueprintIdentifier","SwiftProtobuf::SwiftProtobuf")
+buildableReference.add_attribute("BuildableName","SwiftProtobuf.framework")
+buildableReference.add_attribute("BlueprintName","SwiftProtobuf")
 buildableReference.add_attribute("ReferencedContainer","container:SwiftGRPC-Carthage.xcodeproj")
 
 buildActions.unshift(preActions)
