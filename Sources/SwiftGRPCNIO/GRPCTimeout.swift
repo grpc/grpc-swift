66e8d6e4aac46e9059e02d7c60aa9d7978f4e7d1
George Barnett <gbarnett@apple.com>
2019-02-05 15:21:30 +0000

    GRPCTimeout documentation

--- Sources/SwiftGRPCNIO/GRPCTimeout.swift
+++ Sources/SwiftGRPCNIO/GRPCTimeout.swift
@@ -1,7 +1,96 @@
 import Foundation
 import NIO
 
-public enum GRPCTimeoutUnit: String {
+/// A timeout for a gRPC call.
+///
+/// Timeouts must be positive and at most 8-digits long.
+public struct GRPCTimeout: CustomStringConvertible {
+  /// A description of the timeout in the format described in the
+  /// [gRPC protocol](https://github.com/grpc/grpc/blob/master/doc/PROTOCOL-HTTP2.md).
+  public let description: String
+
+  private let nanoseconds: Int64
+
+  /// Creates a new GRPCTimeout with the given `amount` of the `unit`.
+  ///
+  /// `amount` must be positive and at most 8-digits.
+  private init?(_ amount: Int, _ unit: GRPCTimeoutUnit) {
+    // Timeouts must be positive and at most 8-digits.
+    guard amount >= 0, amount < 100_000_000 else { return nil }
+
+    self.description = "\(amount) \(unit.rawValue)"
+    self.nanoseconds = Int64(amount) * Int64(unit.asNanoseconds)
+  }
+
+  /// Creates a new GRPCTimeout for the given amount of hours.
+  ///
+  /// `amount` must be positive and at most 8-digits.
+  ///
+  /// - Parameter amount: the amount of hours this `GRPCTimeout` represents.
+  /// - Returns: A `GRPCTimeout` representing the given number of hours if the amount was valid, `nil` otherwise.
+  public static func hours(_ amount: Int) -> GRPCTimeout? {
+    return GRPCTimeout(amount, .hours)
+  }
+
+  /// Creates a new GRPCTimeout for the given amount of minutes.
+  ///
+  /// `amount` must be positive and at most 8-digits.
+  ///
+  /// - Parameter amount: the amount of minutes this `GRPCTimeout` represents.
+  /// - Returns: A `GRPCTimeout` representing the given number of minutes if the amount was valid, `nil` otherwise.
+  public static func minutes(_ amount: Int) -> GRPCTimeout? {
+    return GRPCTimeout(amount, .minutes)
+  }
+
+  /// Creates a new GRPCTimeout for the given amount of seconds.
+  ///
+  /// `amount` must be positive and at most 8-digits.
+  ///
+  /// - Parameter amount: the amount of seconds this `GRPCTimeout` represents.
+  /// - Returns: A `GRPCTimeout` representing the given number of seconds if the amount was valid, `nil` otherwise.
+  public static func seconds(_ amount: Int) -> GRPCTimeout? {
+    return GRPCTimeout(amount, .seconds)
+  }
+
+  /// Creates a new GRPCTimeout for the given amount of milliseconds.
+  ///
+  /// `amount` must be positive and at most 8-digits.
+  ///
+  /// - Parameter amount: the amount of milliseconds this `GRPCTimeout` represents.
+  /// - Returns: A `GRPCTimeout` representing the given number of milliseconds if the amount was valid, `nil` otherwise.
+  public static func milliseconds(_ amount: Int) -> GRPCTimeout? {
+    return GRPCTimeout(amount, .milliseconds)
+  }
+
+  /// Creates a new GRPCTimeout for the given amount of microseconds.
+  ///
+  /// `amount` must be positive and at most 8-digits.
+  ///
+  /// - Parameter amount: the amount of microseconds this `GRPCTimeout` represents.
+  /// - Returns: A `GRPCTimeout` representing the given number of microseconds if the amount was valid, `nil` otherwise.
+  public static func microseconds(_ amount: Int) -> GRPCTimeout? {
+    return GRPCTimeout(amount, .microseconds)
+  }
+
+  /// Creates a new GRPCTimeout for the given amount of nanoseconds.
+  ///
+  /// `amount` must be positive and at most 8-digits.
+  ///
+  /// - Parameter amount: the amount of nanoseconds this `GRPCTimeout` represents.
+  /// - Returns: A `GRPCTimeout` representing the given number of nanoseconds if the amount was valid, `nil` otherwise.
+  public static func nanoseconds(_ amount: Int) -> GRPCTimeout? {
+    return GRPCTimeout(amount, .nanoseconds)
+  }
+}
+
+extension GRPCTimeout {
+  /// Returns a NIO `TimeAmount` representing the amount of time as this timeout.
+  public var asNIOTimeAmount: TimeAmount {
+    return TimeAmount.nanoseconds(numericCast(nanoseconds))
+  }
+}
+
+private enum GRPCTimeoutUnit: String {
   case hours = "H"
   case minutes = "M"
   case seconds = "S"
@@ -29,48 +118,5 @@ public enum GRPCTimeoutUnit: String {
     case .nanoseconds:
       return 1
     }
-  }
-}
-
-public struct GRPCTimeout: CustomStringConvertible {
-  public let description: String
-  private let nanoseconds: Int64
-
-  private init?(_ amount: Int, _ unit: GRPCTimeoutUnit) {
-    // Timeouts must be positive and at most 8-digits.
-    guard amount >= 0, amount < 100_000_000 else { return nil }
-
-    self.description = "\(amount) \(unit.rawValue)"
-    self.nanoseconds = Int64(amount) * Int64(unit.asNanoseconds)
-  }
-
-  public static func hours(_ amount: Int) -> GRPCTimeout? {
-    return GRPCTimeout(amount, .hours)
-  }
-
-  public static func minutes(_ amount: Int) -> GRPCTimeout? {
-    return GRPCTimeout(amount, .minutes)
-  }
-
-  public static func seconds(_ amount: Int) -> GRPCTimeout? {
-    return GRPCTimeout(amount, .seconds)
-  }
-
-  public static func milliseconds(_ amount: Int) -> GRPCTimeout? {
-    return GRPCTimeout(amount, .milliseconds)
-  }
-
-  public static func microseconds(_ amount: Int) -> GRPCTimeout? {
-    return GRPCTimeout(amount, .microseconds)
-  }
-
-  public static func nanoseconds(_ amount: Int) -> GRPCTimeout? {
-    return GRPCTimeout(amount, .nanoseconds)
-  }
-}
-
-extension GRPCTimeout {
-  public var asNIOTimeAmount: TimeAmount {
-    return TimeAmount.nanoseconds(numericCast(nanoseconds))
   }
 }
