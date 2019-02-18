395cd424b18fd063a7a8dec46f5f0f2110ddf866
Alexey Gordeev <a.gordeev@dlg.im>
2019-02-18 14:37:35 +0300

    fix #362. Respect PR critique. make roots_pem() public. Update Channel init.

--- Sources/RootsEncoder/main.swift
+++ Sources/RootsEncoder/main.swift
@@ -98,7 +98,7 @@ if CommandLine.arguments.contains("test") {
   s += "import Foundation\n"
   s += "\n"
 
-  s += "func roots_pem() -> String? {\n"
+  s += "public func roots_pem() -> String? {\n"
   let fileURL = URL(fileURLWithPath: "Assets/roots.pem")
   let filedata = try Data(contentsOf: fileURL)
   let encoding = filedata.base64EncodedString()
