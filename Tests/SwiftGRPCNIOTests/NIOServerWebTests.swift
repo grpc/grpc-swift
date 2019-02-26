e39d0a20a5d1f36d487341b704c2f6d266901822
George Barnett <gbarnett@apple.com>
2019-02-26 13:35:16 +0000

    Fix grpc-web testUnaryLotsOfRequests on Linux

--- Tests/SwiftGRPCNIOTests/NIOServerWebTests.swift
+++ Tests/SwiftGRPCNIOTests/NIOServerWebTests.swift
@@ -114,13 +114,15 @@ extension NIOServerWebTests {
     // Sending that many requests at once can sometimes trip things up, it seems.
     let clockStart = clock()
     let numberOfRequests = 2_000
+
     let completionHandlerExpectation = expectation(description: "completion handler called")
-#if os(macOS)
-    // Linux version of Swift doesn't have this API yet.
+    // Linux version of Swift doesn't have the `expectedFulfillmentCount` API yet.
     // Implemented in https://github.com/apple/swift-corelibs-xctest/pull/228 but not yet
     // released.
-    completionHandlerExpectation.expectedFulfillmentCount = numberOfRequests
-#endif
+    //
+    // Wait for the expected number of responses (i.e. `numberOfRequests`) instead.
+    var responses = 0
+
     for i in 0..<numberOfRequests {
       let message = "foo \(i)"
       let expectedData = gRPCEncodedEchoRequest("Swift echo get: \(message)") + gRPCWebOKTrailers()
@@ -129,7 +131,11 @@ extension NIOServerWebTests {
         XCTAssertNil(error)
         if let data = data {
           XCTAssertEqual(String(data: data, encoding: .utf8), expectedResponse)
-          completionHandlerExpectation.fulfill()
+          responses += 1
+
+          if responses == numberOfRequests {
+            completionHandlerExpectation.fulfill()
+          }
         }
       }
     }
