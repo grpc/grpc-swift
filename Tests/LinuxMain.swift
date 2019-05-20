c517e29c325802017027d49cf81f8942b15d14d2
George Barnett <gbarnett@apple.com>
2019-05-20 12:24:07 +0100

    Update LinuxMain

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,10 +1,8 @@
 import XCTest
 
 import SwiftGRPCNIOTests
-import SwiftGRPCTests
 
 var tests = [XCTestCaseEntry]()
 tests += SwiftGRPCNIOTests.__allTests()
-tests += SwiftGRPCTests.__allTests()
 
 XCTMain(tests)
