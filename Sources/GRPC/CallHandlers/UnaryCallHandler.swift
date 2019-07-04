0f42de7bab9483027843ed6467391d8896677a9c
George Barnett <gbarnett@apple.com>
2019-07-04 16:37:37 +0100

    Add missing copyright headers (#505)

--- Sources/GRPC/CallHandlers/UnaryCallHandler.swift
+++ Sources/GRPC/CallHandlers/UnaryCallHandler.swift
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2019, gRPC Authors All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import Foundation
 import SwiftProtobuf
 import NIO
@@ -38,13 +53,13 @@ public class UnaryCallHandler<RequestMessage: Message, ResponseMessage: Message>
       .cascade(to: context.responsePromise)
     self.eventObserver = nil
   }
-  
+
   public override func endOfStreamReceived() throws {
     if self.eventObserver != nil {
       throw GRPCError.server(.noRequestsButOneExpected)
     }
   }
-  
+
   override func sendErrorStatus(_ status: GRPCStatus) {
     callContext?.responsePromise.fail(status)
   }
