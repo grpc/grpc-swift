0a0f32b7eb88de75c9e83d6628005a4db1f13e79
George Barnett <gbarnett@apple.com>
2019-09-25 14:18:15 +0100

    Add a RouteGuide example and tutorial
    Motivation:
    
    a30043ad (#590) introduced a 'Hello World' example and tutorial to help
    new gRPC Swift users. A more in-depth tutorial would be useful as a
    next-step for those users.
    
    Modifications:
    
    Add a tutorial based on the "route guide" service as per
    https://grpc.io/docs/tutorials/
    
    Move parts of the `README.md` into separate documenation files in `docs/`
    
    Result:
    
    - Another gRPC tutorial
    - Easier to find documenation

---
+++ Sources/Examples/RouteGuide/README.md
@@ -0,0 +1,33 @@
+# Route Guide - A sample gRPC Application
+
+This directory contains the source and generated code for the gRPC "Route Guide"
+example.
+
+The tutorial relating to this example can be found in
+[grpc-swift/docs/basic-tutorial.md][basic-tutorial].
+
+## Running
+
+To start the server, from the root of this package run:
+
+```sh
+$ swift run RouteGuideServer
+```
+
+Once the server has started it will print out the port is listening on. This
+must be passed to the client.
+
+```sh
+$ swift run RouteGuideClient <PORT>
+```
+
+## Regenerating client and server code
+
+For simplicity, a Makefile is provided in the root of this package with a target
+to generate client and server code:
+
+```sh
+$ make generate-route-guide
+```
+
+[basic-tutorial]: ../../../docs/basic-tutorial.md
---
+++ Sources/Examples/RouteGuide/Server/main.swift
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2019, gRPC Authors All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import Foundation
+import GRPC
+import RouteGuideModel
+import NIO
+import Logging
+import SwiftProtobuf
+
+// Quieten the logs.
+LoggingSystem.bootstrap {
+  var handler = StreamLogHandler.standardOutput(label: $0)
+  handler.logLevel = .critical
+  return handler
+}
+
+/// Loads the features from `route_guide_db.json`, assumed to be in the directory above this file.
+func loadFeatures() throws -> [Routeguide_Feature] {
+  let url = URL(fileURLWithPath: #file)
+    .deletingLastPathComponent()  // main.swift
+    .deletingLastPathComponent()  // Server/
+    .appendingPathComponent("route_guide_db.json")
+
+  let data = try Data(contentsOf: url)
+  return try Routeguide_Feature.array(fromJSONUTF8Data: data)
+}
+
+func main(args: [String]) throws {
+  // Create an event loop group for the server to run on.
+  let group = MultiThreadedEventLoopGroup(numberOfThreads: System.coreCount)
+  defer {
+    try! group.syncShutdownGracefully()
+  }
+
+  // Read the feature database.
+  let features = try loadFeatures()
+
+  // Create a provider using the features we read.
+  let provider = RouteGuideProvider(features: features)
+
+  // Tie these together in some configuration:
+  let configuration = Server.Configuration(
+    target: .hostAndPort("localhost", 0),
+    eventLoopGroup: group,
+    serviceProviders: [provider]
+  )
+
+  // Start the server and print its address once it has started.
+  let server = Server.start(configuration: configuration)
+  server.map {
+    $0.channel.localAddress
+  }.whenSuccess { address in
+    print("server started on port \(address!.port!)")
+  }
+
+  // Wait on the server's `onClose` future to stop the program from exiting.
+  _ = try server.flatMap {
+    $0.onClose
+  }.wait()
+}
+
+try main(args: CommandLine.arguments)
