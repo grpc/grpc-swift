2741421b91fe806ffe39f8f69ebf036d654fb6f9
Prickett <jpricke1@ford.com>
2020-04-03 13:10:13 -0400

    Add build_podspec script

---
+++ scripts/build_podspecs.py
@@ -0,0 +1,172 @@
+# Copyright 2020, gRPC Authors All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import json
+import random
+import string
+import argparse
+
+class Dependency:
+    def __init__(self, name, version="s.version.to_s", internal=True):
+        self.name = name
+        self.version = version
+        self.internal = internal
+
+    def as_podspec(self):
+        if self.internal:
+            return "    s.dependency '%s', %s\n" % (self.name, self.version)
+        else: 
+            return "    s.dependency '%s', '%s'\n" % (self.name, self.version)
+
+class Pod:
+    def __init__(self, name, module_name, version, dependencies=[]):
+        self.name = name
+        self.module_name = module_name
+        self.version = version
+        self.dependencies = dependencies
+    
+    def add_dependency(self, dependency):
+        self.dependencies.append(dependency)
+    
+    def as_podspec(self):
+        print('\n')
+        print('Building Podspec for %s' % self.name)
+        print('-----------------------------------------------------------')
+
+        podspec = "Pod::Spec.new do |s|\n\n"
+        podspec += "    s.name = '%s'\n" % self.name
+        podspec += "    s.module_name = '%s'\n" % self.module_name
+        podspec += "    s.version = '%s'\n" % self.version
+        podspec += "    s.license = { :type => 'Apache 2.0', :file => 'LICENSE' }\n"
+        podspec += "    s.summary = 'Swift gRPC code generator plugin and runtime library'\n"
+        podspec += "    s.homepage = 'https://www.grpc.io'\n"
+        podspec += "    s.authors  = { 'The gRPC contributors' => 'grpc-packages@google.com' }\n\n"
+
+        podspec += "    s.source = { :git => 'https://github.com/grpc/grpc-swift.git', :tag => s.version }\n\n"
+
+        podspec += "    s.swift_version = '5.0'\n"
+
+        podspec += "    s.ios.deployment_target = '10.0'\n"
+        podspec += "    s.osx.deployment_target = '10.12'\n"
+        podspec += "    s.tvos.deployment_target = '10.0'\n"
+        
+        podspec += "    s.source_files = 'Sources/%s/**/*.{swift,c,h}'\n" % (self.module_name)
+
+        podspec += "\n" if len(self.dependencies) > 0 else ""
+
+        for dep in self.dependencies:
+            podspec += dep.as_podspec()
+
+        podspec += "\nend"
+        return podspec
+
+class PodManager:
+    pods = []
+
+    def __init__(self, directory, version, should_publish):
+        self.directory = directory
+        self.version = version
+        self.should_publish = should_publish
+
+    def write(self, pod, contents):
+        print("    Writing to %s/%s.podspec " % (self.directory, pod))
+        f = open("%s/%s.podspec" % (self.directory, pod), "w")
+        f.write(contents)
+        f.close
+    
+    def publish(self, pod_name):
+        os.system('pod repo update')
+        print("    Publishing %s.podspec" % (pod_name))
+        os.system('pod repo push %s/%s.podspec' % (self.directory, pod_name))
+    
+    def build_pods(self):
+        CGRPCZlibPod = Pod('CGRPCZlib', 'CGRPCZlib', self.version)
+
+        GRPCPod = Pod('gRPC-Swift', 'GRPC', self.version, get_grpc_deps())
+        GRPCPod.add_dependency(Dependency('CGRPCZlib'))
+
+        self.pods.append(CGRPCZlibPod)
+        self.pods.append(GRPCPod)
+
+    def go(self):
+        self.build_pods()
+        # Create .podspec files and publish
+        for target in self.pods:
+            self.write(target.name, target.as_podspec())
+            if self.should_publish:
+                self.publish(target.name)
+            else:
+                print("    Skipping Publishing...")
+
+def process_package(string):
+    if string == "swift-log":
+        return "Logging"
+
+    if len(string.split('-')) > 1:
+        tempList = string.split('-')
+        returnStr = ""
+        for item in tempList:
+            if item == 'nio' or item == 'ssl' or item == 'http2':
+                returnStr += item.upper()
+            else:
+                returnStr += item.title()
+        return returnStr
+    
+    return string
+
+def get_grpc_deps():
+    with open('Package.resolved') as f:
+        data = json.load(f)
+
+    deps = []
+
+    for obj in data["object"]["pins"]:
+        package = process_package(obj["package"])
+        version = obj["state"]["version"]
+
+        deps.append(Dependency(package, version, False))
+
+    return deps
+
+def main():
+
+    # Setup
+
+    parser = argparse.ArgumentParser(description='Build Podspec files for SwiftGRPC')
+    
+    parser.add_argument(
+        '-u', 
+        '--upload',
+        action='store_true',
+        help='Determines if the newly built Podspec files should be pushed.'
+        )
+    
+    parser.add_argument('version')
+
+    args = parser.parse_args()
+
+    should_publish = args.upload
+    version = args.version
+
+    # Creates temp folder /tmp/.argonaut_podspecsXXXXXX for storage
+    #    XXXXXX is a random alpha numeric string to ensure we don't have duplicates
+    path = "/tmp/.build_podspecs" + ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(6))
+    os.mkdir(path)
+
+    pod_manager = PodManager(path, version, should_publish)
+    pod_manager.go()
+
+if __name__ == "__main__":
+    main()
