8c446083d142e28d1f1d9572aa9233dfbd9638ef
Prickett <jpricke1@ford.com>
2020-05-03 21:20:14 -0400

    Adjust implementation based on PR feedback

--- Examples/Google/SpeechToText/README.md
+++ Examples/Google/SpeechToText/README.md
@@ -18,11 +18,16 @@ This project requires a Google Cloud API Key. Please [register](https://cloud.go
 2. Navigate to the root directory (`Examples/Google/SpeechToText`) and run `pod install`
 3. Run `make protos` to pull the most recent .proto files from the googleapis repository
 4. run `make generate` to leverage the `protoc` plugin to generate the Swift interfaces
-5. Open the `.xworkspace`
+    - Note: Please refer to [Getting the `protoc` Plugins](https://github.com/grpc/grpc-swift#getting-the-protoc-plugins). For this tutorial, the Makefile requires that the `protoc-gen-grpc-swift` and `protoc-gen-swift` are on the users PATH.
+5. Open the `.xcworkspace`
 6. Open the `Constants.swift` file and assign your generated Google Cloud API Key to the `kAPIKey` variable.
+    - Note: Once you add the API Key, feel free to remove the forced warning/reminder on line 23.
 7. Run the application!
 
 ## Helpful Links
 * [Getting Started with Speech APIs](https://cloud.google.com/speech-to-text/docs/quickstart)
 * [CocoaPods](https://cocoapods.org/)
 * [gRPC-Swift CocoaPod](https://cocoapods.org/pods/gRPC-Swift)
+
+NOTE: Implementation of the AudioStreamManager is based off of [Google's Example Audio Streaming](https://github.com/GoogleCloudPlatform/ios-docs-samples/blob/master/speech/Swift/Speech-gRPC-Streaming/Speech/AudioController.swift)
+
--- Examples/Google/SpeechToText/Sources/SpeechService.swift
+++ Examples/Google/SpeechToText/Sources/SpeechService.swift
@@ -1,98 +1,122 @@
-//
-//  SpeechService.swift
-//  SpeechToText-gRPC-iOS
-//
-//  Created by Prickett, Jacob (J.A.) on 4/13/20.
-//  Copyright © 2020 Prickett, Jacob (J.A.). All rights reserved.
-//
+/*
+ * Copyright 2020, gRPC Authors All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import GRPC
 
+typealias Request = Google_Cloud_Speech_V1_StreamingRecognizeRequest
+typealias Response = Google_Cloud_Speech_V1_StreamingRecognizeResponse
+typealias StreamingRecognizeCall = BidirectionalStreamingCall
+
+// Track whether we are currently streaming or not
+enum State {
+  case idle
+  case streaming(StreamingRecognizeCall<Request, Response>)
+}
+
 final class SpeechService {
+  
+  // Generated SpeechClient for making calls
+  private var client: Google_Cloud_Speech_V1_SpeechClient
+  
+  // Track if we are streaming or not
+  private var state: State = .idle
+  
+  init() {
+    precondition(!Constants.apiKey.isEmpty, "Please refer to the README on how to configure your API Key properly.")
+    
+    // Make EventLoopGroup for the specific platform (NIOTSEventLoopGroup for iOS)
+    let group = PlatformSupport.makeEventLoopGroup(loopCount: 1)
+    
+    // Create connection channel with our group, host, and port
+    let channel = ClientConnection
+      .secure(group: group)
+      .connect(host: "speech.googleapis.com", port: 443)
 
-    // Generated SpeechClient for making calls
-    private var client: Google_Cloud_Speech_V1_SpeechClient
+    // Specify call options to be used for gRPC calls
+    let callOptions = CallOptions(customMetadata: [
+      "x-goog-api-key": Constants.apiKey
+    ])
 
-    // Stream that is returned from `streamingRecognize` call
-    private var call: BidirectionalStreamingCall<Google_Cloud_Speech_V1_StreamingRecognizeRequest, Google_Cloud_Speech_V1_StreamingRecognizeResponse>?
+    // Now we have a client!
+    self.client = Google_Cloud_Speech_V1_SpeechClient(channel: channel, defaultCallOptions: callOptions)
+  }
+  
+  func stream(_ data: Data,
+              completion: ((Google_Cloud_Speech_V1_StreamingRecognizeResponse) -> Void)? = nil) {
 
-    // Track if we are streaming or not
-    private var isStreaming: Bool = false
+    switch self.state {
+    case .idle:
+      // Initialize the bidirectional stream
+      let call = self.client.streamingRecognize { response in
+        // Message received from Server, execute provided closure from caller
+        completion?(response)
+      }
 
-    init() {
-
-        // Specify call options to be used for gRPC calls
-        var callOptions = CallOptions()
-
-        // API Key
-        callOptions.customMetadata.add(name: "X-Goog-Api-Key",
-                                       value: Constants.kAPIKey)
-
-        // Specific Bundle Id for the API Key
-        callOptions.customMetadata.add(name: "X-Ios-Bundle-Identifier",
-                                       value: "com.ford.SpeechToText-gRPC-iOS")
-
-        // Make EventLoopGroup for the specific platform (NIOTSEventLoopGroup for iOS)
-        let group = PlatformSupport.makeEventLoopGroup(loopCount: 1)
-
-        // Create connection channel with our group, host, and port
-        let channel = ClientConnection
-            .secure(group: group)
-            .connect(host: "speech.googleapis.com", port: 443)
-
-        // Now we have a client!
-        client = Google_Cloud_Speech_V1_SpeechClient(channel: channel, defaultCallOptions: callOptions)
-    }
-
-    func stream(_ data: Data,
-                completion: ((Google_Cloud_Speech_V1_StreamingRecognizeResponse) -> Void)? = nil) {
-        // If we aren't already streaming
-        if !isStreaming {
-
-            // Initialize the bidirectional stream
-            call = client.streamingRecognize { (response) in
-                // Message received from Server, execute provided closure from caller
-                completion?(response)
-            }
-
-            isStreaming = true
-
-            // Specify audio details
-            let config = Google_Cloud_Speech_V1_RecognitionConfig.with {
-                $0.encoding = .linear16
-                $0.sampleRateHertz = 16000
-                $0.languageCode = "en-US"
-                $0.metadata = Google_Cloud_Speech_V1_RecognitionMetadata.with {
-                    $0.interactionType = .dictation
-                    $0.microphoneDistance = .nearfield
-                    $0.recordingDeviceType = .smartphone
-                }
-            }
-
-            // Create streaming request
-            let request = Google_Cloud_Speech_V1_StreamingRecognizeRequest.with {
-                $0.streamingConfig = Google_Cloud_Speech_V1_StreamingRecognitionConfig.with {
-                    $0.config = config
-                }
-            }
-
-            // Send first message consisting of the streaming request details
-            _ = call?.sendMessage(request)
+      // Specify audio details
+      let config = Google_Cloud_Speech_V1_RecognitionConfig.with {
+        $0.encoding = .linear16
+        $0.sampleRateHertz = 16000
+        $0.languageCode = "en-US"
+        $0.enableAutomaticPunctuation = true
+        $0.metadata = Google_Cloud_Speech_V1_RecognitionMetadata.with {
+          $0.interactionType = .dictation
+          $0.microphoneDistance = .nearfield
+          $0.recordingDeviceType = .smartphone
         }
+      }
 
-        // Stream request to send that contains the audio details
-        let streamAudioDataRequest = Google_Cloud_Speech_V1_StreamingRecognizeRequest.with {
-            $0.audioContent = data
+      // Create streaming request
+      let request = Google_Cloud_Speech_V1_StreamingRecognizeRequest.with {
+        $0.streamingConfig = Google_Cloud_Speech_V1_StreamingRecognitionConfig.with {
+          $0.config = config
         }
+      }
 
-        // Send audio data
-        _ = call?.sendMessage(streamAudioDataRequest)
+      // Send first message consisting of the streaming request details
+      call.sendMessage(request, promise: nil)
+
+      self.state = .streaming(call)
+
+      // Stream request to send that contains the audio details
+      let streamAudioDataRequest = Google_Cloud_Speech_V1_StreamingRecognizeRequest.with {
+        $0.audioContent = data
+      }
+
+      // Send audio data
+      call.sendMessage(streamAudioDataRequest, promise: nil)
+
+    case .streaming(let stream):
+      // Stream request to send that contains the audio details
+      let streamAudioDataRequest = Google_Cloud_Speech_V1_StreamingRecognizeRequest.with {
+        $0.audioContent = data
+      }
+
+      // Send audio data
+      stream.sendMessage(streamAudioDataRequest, promise: nil)
     }
-
-    func stopStreaming() {
-        // Send end message to the stream
-        _ = call?.sendEnd()
-        isStreaming.toggle()
+  }
+  
+  func stopStreaming() {
+    // Send end message to the stream
+    switch self.state {
+    case .idle:
+      return
+    case .streaming(let stream):
+      stream.sendEnd(promise: nil)
+      self.state = .idle
     }
-
+  }
 }
