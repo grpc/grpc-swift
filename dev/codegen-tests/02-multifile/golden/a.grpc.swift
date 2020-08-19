fab6020feb5716ce945cf20a2af32cfd78709243
George Barnett <gbarnett@apple.com>
2020-08-19 09:03:43 +0100

    Regenerate

--- dev/codegen-tests/02-multifile/golden/a.grpc.swift
+++ dev/codegen-tests/02-multifile/golden/a.grpc.swift
@@ -20,11 +20,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-import Foundation
 import GRPC
 import NIO
-import NIOHTTP1
-import SwiftProtobuf
 
 
 /// Usage: instantiate A_ServiceAClient, then call methods of this protocol to make API calls.
--- dev/codegen-tests/03-multifile-with-module-map/golden/a.grpc.swift
+++ dev/codegen-tests/03-multifile-with-module-map/golden/a.grpc.swift
@@ -20,11 +20,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-import Foundation
 import GRPC
 import NIO
-import NIOHTTP1
-import SwiftProtobuf
 import ModuleB
 
 
