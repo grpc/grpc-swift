c9df463079510ef0a11a9d07595881c05070725f
George Barnett <gbarnett@apple.com>
2020-10-13 09:52:04 +0100

    Add scaffolding for client interceptors (#986)
    Motivation:
    
    To support client interceptors we first need to define a few types to
    work with.
    
    Modifications:
    
    - Add client request and response parts.
    - Add the client interceptor protocol.
    - Add the client interceptor context, which is passed to functions on
      the interceptor protocol in order to allow implementations to invoke
      functions on interceptors either side of it in the pipeline.
    - Add a stubbed out client interceptor pipeline.
    
    Result:
    
    We have some base types for client side interceptors.

---
+++ Sources/GRPC/Interceptor/ClientInterceptor.swift
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2020, gRPC Authors All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import NIO
+
+internal protocol ClientInterceptor {
+  associatedtype Request
+  associatedtype Response
+
+  /// Called when the interceptor has received a response part to handle.
+  func read(
+    _ part: ClientResponsePart<Response>,
+    context: ClientInterceptorContext<Request, Response>
+  )
+
+  /// Called when the interceptor has received a request part to handle.
+  func write(
+    _ part: ClientRequestPart<Request>,
+    promise: EventLoopPromise<Void>?,
+    context: ClientInterceptorContext<Request, Response>
+  )
+
+  /// Called when the interceptor has received a request to cancel the RPC.
+  func cancel(
+    promise: EventLoopPromise<Void>?,
+    context: ClientInterceptorContext<Request, Response>
+  )
+}
