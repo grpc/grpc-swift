85a78c1bea6b805e82331be1f55bfa953e7fb586
George Barnett <gbarnett@apple.com>
2020-10-15 14:02:04 +0100

    Add head and tail interceptors. (#993)
    Motivation:
    
    An interceptor pipeline needs interceptors, and users need to be able to
    create their own interceptors.
    
    Modifications:
    
    - Rename 'ClientInterceptor' to 'ClientInterceptorProtocol'
    - Add a 'ClientInterceptor' base class for users to extend
    - Add head and tail interceptors
    - Add 'AnyClientInterceptor' which wraps the above.
    - Build out a little more of the pipeline; note that this is by no means
      complete yet but it gets us to a stage where we can fire events
      through it.
    - Move next inbound/outbound context fetching from context to pipeline
      because the context doesn't need to know the ordering of interceptors.
    - Add a couple of simple pipeline tests.
    
    Result:
    
    We have a basic interceptor pipeline and a way for users to define
    interceptors.

--- Sources/GRPC/Interceptor/ClientInterceptorPipeline.swift
+++ Sources/GRPC/Interceptor/ClientInterceptorPipeline.swift
@@ -37,15 +37,15 @@ import NIOHTTP2
 /// │ ┌──────────────────────────────────────────────────────▼────────┐ │
 /// │ │     Tail Interceptor (hands response parts to a callback)     │ │
 /// │ └────────▲─────────────────────────────────────────────┬────────┘ │
-/// │          ╎                                             ╎          │
-/// │          ╎              (More interceptors)            ╎          │
-/// │          ╎                                             ╎          │
-/// │ ┌────────┴─────────────────────────────────────────────▼────────┐ │
-/// │ │                          Interceptor 2                        │ │
-/// │ └────────▲─────────────────────────────────────────────┬────────┘ │
 /// │ ┌────────┴─────────────────────────────────────────────▼────────┐ │
 /// │ │                          Interceptor 1                        │ │
 /// │ └────────▲─────────────────────────────────────────────┬────────┘ │
+/// │ ┌────────┴─────────────────────────────────────────────▼────────┐ │
+/// │ │                          Interceptor 2                        │ │
+/// │ └────────▲─────────────────────────────────────────────┬────────┘ │
+/// │          ╎                                             ╎          │
+/// │          ╎              (More interceptors)            ╎          │
+/// │          ╎                                             ╎          │
 /// │ ┌────────┴─────────────────────────────────────────────▼────────┐ │
 /// │ │          Head Interceptor (interacts with transport)          │ │
 /// │ └────────▲─────────────────────────────────────────────┬────────┘ │
@@ -65,30 +65,97 @@ internal final class ClientInterceptorPipeline<Request, Response> {
   internal let eventLoop: EventLoop
 
   /// The contexts associated with the interceptors stored in this pipeline. Context will be removed
-  /// once the RPC has completed.
-  private var contexts: [ClientInterceptorContext<Request, Response>]
+  /// once the RPC has completed. Contexts are ordered from outbound to inbound, that is, the tail
+  /// is first and the head is last.
+  private var contexts: [ClientInterceptorContext<Request, Response>]?
+
+  /// Returns the next context in the outbound direction for the context at the given index, if one
+  /// exists.
+  /// - Parameter index: The index of the `ClientInterceptorContext` which is requesting the next
+  ///   outbound context.
+  /// - Returns: The `ClientInterceptorContext` or `nil` if one does not exist.
+  internal func nextOutboundContext(
+    forIndex index: Int
+  ) -> ClientInterceptorContext<Request, Response>? {
+    return self.context(atIndex: index + 1)
+  }
+
+  /// Returns the next context in the inbound direction for the context at the given index, if one
+  /// exists.
+  /// - Parameter index: The index of the `ClientInterceptorContext` which is requesting the next
+  ///   inbound context.
+  /// - Returns: The `ClientInterceptorContext` or `nil` if one does not exist.
+  internal func nextInboundContext(
+    forIndex index: Int
+  ) -> ClientInterceptorContext<Request, Response>? {
+    return self.context(atIndex: index - 1)
+  }
 
   /// Returns the context for the given index, if one exists.
   /// - Parameter index: The index of the `ClientInterceptorContext` to return.
   /// - Returns: The `ClientInterceptorContext` or `nil` if one does not exist for the given index.
-  internal func context(atIndex index: Int) -> ClientInterceptorContext<Request, Response>? {
-    return self.contexts[checked: index]
+  private func context(atIndex index: Int) -> ClientInterceptorContext<Request, Response>? {
+    return self.contexts?[checked: index]
   }
 
   /// The context closest to the `NIO.Channel`, i.e. where inbound events originate. This will be
   /// `nil` once the RPC has completed.
   private var head: ClientInterceptorContext<Request, Response>? {
-    return self.contexts.first
+    return self.contexts?.last
   }
 
   /// The context closest to the application, i.e. where outbound events originate. This will be
   /// `nil` once the RPC has completed.
   private var tail: ClientInterceptorContext<Request, Response>? {
-    return self.contexts.last
+    return self.contexts?.first
   }
 
-  internal init() {
-    fatalError("Not yet implemented.")
+  internal init(
+    logger: Logger,
+    eventLoop: EventLoop,
+    interceptors: [ClientInterceptor<Request, Response>],
+    errorDelegate: ClientErrorDelegate?,
+    onCancel: @escaping (EventLoopPromise<Void>?) -> Void,
+    onRequestPart: @escaping (ClientRequestPart<Request>, EventLoopPromise<Void>?) -> Void,
+    onResponsePart: @escaping (ClientResponsePart<Response>) -> Void
+  ) {
+    self.eventLoop = eventLoop
+    self.logger = logger
+
+    // We know we'll have at least a head and a tail as well as any user provided interceptors.
+    var contexts: [ClientInterceptorContext<Request, Response>] = []
+    contexts.reserveCapacity(interceptors.count + 2)
+
+    // Start with the tail.
+    contexts.append(
+      ClientInterceptorContext(
+        for: .tail(for: self, errorDelegate: errorDelegate, onResponsePart),
+        atIndex: contexts.count,
+        in: self
+      )
+    )
+
+    // Now the user interceptors.
+    for interceptor in interceptors {
+      contexts.append(
+        ClientInterceptorContext(
+          for: .userProvided(interceptor),
+          atIndex: contexts.count,
+          in: self
+        )
+      )
+    }
+
+    // Finally, the head.
+    contexts.append(
+      ClientInterceptorContext(
+        for: .head(onCancel: onCancel, onRequestPart: onRequestPart),
+        atIndex: contexts.count,
+        in: self
+      )
+    )
+
+    self.contexts = contexts
   }
 
   /// Emit a response part message into the interceptor pipeline.
@@ -134,5 +201,19 @@ internal final class ClientInterceptorPipeline<Request, Response> {
     } else {
       promise?.fail(GRPCStatus(code: .unavailable, message: "The RPC has already completed"))
     }
+  }
+}
+
+// MARK: - Lifecycle
+
+extension ClientInterceptorPipeline {
+  /// Closes the pipeline. This should be called once, by the tail interceptor, to indicate that
+  /// the RPC has completed.
+  /// - Important: This *must* to be called from the `eventLoop`.
+  internal func close() {
+    self.eventLoop.assertInEventLoop()
+
+    // TODO: make sure the transport is closed (in case a user interceptor emits an error).
+    self.contexts = nil
   }
 }
