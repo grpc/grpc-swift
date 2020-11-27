grpc/grpc-swift

commit 248f554e3ce81dec535f6aad18dbc9a85ad5d913

Support protobuf separately to `GRPCPayload` for the client (#889)
Motivation:

To support payloads other than `SwiftProtobuf.Message` we required that
all messages conform to `GRPCPayload`. For protobuf messages we added
`GRPCProtobufPayload` which provides a default implemenation of
`GRPCPayload` for protobuf messages. We generated this conformance for
all protobuf messages we saw. This lead to a number issues and
workarounds including: #738, #778, #801, #837, #877, #881.

The intention is to continue to support `GRPCPayload` in addition to
protobuf, however, support for protobuf will not be via the
`GRPCProtobufPayload` protocol.

This PR builds on #886 by increasing the surface area of the client APIs
so that they are not constrained to `GRPCPayload`. The surface API now
has variants for `GRPCPayload` and `SwiftProtobuf.Message`. Internally
the client deals with serializers and deserializers.

Modifications:

- `GRPCClientChannelHandler` and `GRPCClientStateMachine` are no longer
  generic over a request and response type, rather they deal with the
  serialzed version of requests and response (i.e. `ByteBuffer`s) and
  defer the (de/)serialization to a separate handler.
- Added `GRCPClientCodecHandler` to handle (de/)serialization of
  messages
- Clients are no longer constrained to having their request/response
  payloads conform to `GRPCPayload`
- Conformance to `GRPCProtobufPayload` is no longer generated and the
  protocol is deprecated and has no requirements.
- Drop the 'GenerateConformance' option from the codegen since it is no
  longer required
- Reintroduce a filter to the codegen so that we only consider files
  which contain services, this avoids generating empty files
- Regenerate code where necessary

Result:

- `GRPCProtobufPayload` is no longer required

Sources/protoc-gen-grpc-swift/main.swift
@@ -102,10 +102,8 @@ func main() throws {
   // Build the SwiftProtobufPluginLibrary model of the plugin input
   let descriptorSet = DescriptorSet(protos: request.protoFile)
 
-  // process each .proto file in filename order in an attempt to stabilise the output (i.e. where
-  // conformance to `GRPCPayload` is generated)
-  for name in request.fileToGenerate.sorted() {
-    let fileDescriptor = descriptorSet.lookupFileDescriptor(protoName: name)
+  // Only generate output for services.
+  for fileDescriptor in descriptorSet.files where !fileDescriptor.services.isEmpty {
     let grpcFileName = uniqueOutputFileName(component: "grpc", fileDescriptor: fileDescriptor, fileNamingOption: options.fileNaming)
     let grpcGenerator = Generator(fileDescriptor, options: options)
     var grpcFile = Google_Protobuf_Compiler_CodeGeneratorResponse.File()

Sources/Examples/Echo/Model/echo.grpc.swift
@@ -306,7 +306,3 @@ extension Echo_EchoProvider {
   }
 }
 
-
-// Provides conformance to `GRPCPayload`
-extension Echo_EchoRequest: GRPCProtobufPayload {}
-extension Echo_EchoResponse: GRPCProtobufPayload {}

Sources/Examples/RouteGuide/Model/route_guide.grpc.swift
@@ -223,10 +223,3 @@ extension Routeguide_RouteGuideProvider {
   }
 }
 
-
-// Provides conformance to `GRPCPayload`
-extension Routeguide_Point: GRPCProtobufPayload {}
-extension Routeguide_Rectangle: GRPCProtobufPayload {}
-extension Routeguide_Feature: GRPCProtobufPayload {}
-extension Routeguide_RouteNote: GRPCProtobufPayload {}
-extension Routeguide_RouteSummary: GRPCProtobufPayload {}

Sources/GRPC/ClientCalls/BidirectionalStreamingCall.swift
@@ -22,10 +22,7 @@ import Logging
 ///
 /// Messages should be sent via the `sendMessage` and `sendMessages` methods; the stream of messages
 /// must be terminated by calling `sendEnd` to indicate the final message has been sent.
-public final class BidirectionalStreamingCall<
-  RequestPayload: GRPCPayload,
-  ResponsePayload: GRPCPayload
->: StreamingRequestClientCall {
+public final class BidirectionalStreamingCall<RequestPayload, ResponsePayload>: StreamingRequestClientCall {
   private let transport: ChannelTransport<RequestPayload, ResponsePayload>
 
   /// The options used to make the RPC.
@@ -147,16 +144,20 @@ public final class BidirectionalStreamingCall<
 }
 
 extension BidirectionalStreamingCall {
-  internal static func makeOnHTTP2Stream(
+  internal static func makeOnHTTP2Stream<Serializer: MessageSerializer, Deserializer: MessageDeserializer>(
     multiplexer: EventLoopFuture<HTTP2StreamMultiplexer>,
+    serializer: Serializer,
+    deserializer: Deserializer,
     callOptions: CallOptions,
     errorDelegate: ClientErrorDelegate?,
     logger: Logger,
     responseHandler: @escaping (ResponsePayload) -> Void
-  ) -> BidirectionalStreamingCall<RequestPayload, ResponsePayload> {
+  ) -> BidirectionalStreamingCall<RequestPayload, ResponsePayload> where Serializer.Input == RequestPayload, Deserializer.Output == ResponsePayload {
     let eventLoop = multiplexer.eventLoop
     let transport = ChannelTransport<RequestPayload, ResponsePayload>(
       multiplexer: multiplexer,
+      serializer: serializer,
+      deserializer: deserializer,
       responseContainer: .init(eventLoop: eventLoop, streamingResponseHandler: responseHandler),
       callType: .bidirectionalStreaming,
       timeLimit: callOptions.timeLimit,
@@ -167,12 +168,14 @@ extension BidirectionalStreamingCall {
     return BidirectionalStreamingCall(transport: transport, options: callOptions)
   }
 
-  internal static func make(
+  internal static func make<Serializer: MessageSerializer, Deserializer: MessageDeserializer>(
+    serializer: Serializer,
+    deserializer: Deserializer,
     fakeResponse: FakeStreamingResponse<RequestPayload, ResponsePayload>?,
     callOptions: CallOptions,
     logger: Logger,
     responseHandler: @escaping (ResponsePayload) -> Void
-  ) -> BidirectionalStreamingCall<RequestPayload, ResponsePayload> {
+  ) -> BidirectionalStreamingCall<RequestPayload, ResponsePayload> where Serializer.Input == RequestPayload, Deserializer.Output == ResponsePayload {
     let eventLoop = fakeResponse?.channel.eventLoop ?? EmbeddedEventLoop()
     let responseContainer = ResponsePartContainer(eventLoop: eventLoop, streamingResponseHandler: responseHandler)
 

Sources/GRPC/ClientCalls/ClientCall.swift
@@ -23,9 +23,9 @@ import SwiftProtobuf
 /// Base protocol for a client call to a gRPC service.
 public protocol ClientCall {
   /// The type of the request message for the call.
-  associatedtype RequestPayload: GRPCPayload
+  associatedtype RequestPayload
   /// The type of the response message for the call.
-  associatedtype ResponsePayload: GRPCPayload
+  associatedtype ResponsePayload
 
   /// The event loop this call is running on.
   var eventLoop: EventLoop { get }
@@ -159,7 +159,7 @@ public protocol UnaryResponseClientCall: ClientCall {
 // a NIO HTTP/2 stream channel.
 
 internal protocol ClientCallInbound {
-  associatedtype Response: GRPCPayload
+  associatedtype Response
   typealias ResponsePart = _GRPCClientResponsePart<Response>
 
   /// Receive an error.
@@ -170,7 +170,7 @@ internal protocol ClientCallInbound {
 }
 
 internal protocol ClientCallOutbound {
-  associatedtype Request: GRPCPayload
+  associatedtype Request
   typealias RequestPart = _GRPCClientRequestPart<Request>
 
   /// Send a single request part and complete the promise once the part has been sent.

Sources/GRPC/ClientCalls/ClientCallTransport.swift
@@ -44,7 +44,7 @@ import Logging
 ///```
 ///
 /// Note: the "main" pipeline provided by the channel in `ClientConnection`.
-internal class ChannelTransport<Request: GRPCPayload, Response: GRPCPayload> {
+internal class ChannelTransport<Request, Response> {
   internal typealias RequestPart = _GRPCClientRequestPart<Request>
   internal typealias ResponsePart = _GRPCClientResponsePart<Response>
 
@@ -148,14 +148,16 @@ internal class ChannelTransport<Request: GRPCPayload, Response: GRPCPayload> {
     channelProvider(self, channelPromise)
   }
 
-  internal convenience init(
+  internal convenience init<Serializer: MessageSerializer, Deserializer: MessageDeserializer>(
     multiplexer: EventLoopFuture<HTTP2StreamMultiplexer>,
+    serializer: Serializer,
+    deserializer: Deserializer,
     responseContainer: ResponsePartContainer<Response>,
     callType: GRPCCallType,
     timeLimit: TimeLimit,
     errorDelegate: ClientErrorDelegate?,
     logger: Logger
-  ) {
+  ) where Serializer.Input == Request, Deserializer.Output == Response {
     self.init(
       eventLoop: multiplexer.eventLoop,
       responseContainer: responseContainer,
@@ -168,7 +170,8 @@ internal class ChannelTransport<Request: GRPCPayload, Response: GRPCPayload> {
         case .success(let mux):
           mux.createStreamChannel(promise: streamPromise) { stream, streamID in
             stream.pipeline.addHandlers([
-              _GRPCClientChannelHandler<Request, Response>(streamID: streamID, callType: callType, logger: logger),
+              _GRPCClientChannelHandler(streamID: streamID, callType: callType, logger: logger),
+              GRPCClientCodecHandler(serializer: serializer, deserializer: deserializer),
               GRPCClientCallHandler(call: call)
             ])
           }

Sources/GRPC/ClientCalls/ClientStreamingCall.swift
@@ -22,10 +22,7 @@ import Logging
 ///
 /// Messages should be sent via the `sendMessage` and `sendMessages` methods; the stream of messages
 /// must be terminated by calling `sendEnd` to indicate the final message has been sent.
-public final class ClientStreamingCall<
-  RequestPayload: GRPCPayload,
-  ResponsePayload: GRPCPayload
-> : StreamingRequestClientCall, UnaryResponseClientCall {
+public final class ClientStreamingCall<RequestPayload, ResponsePayload>: StreamingRequestClientCall, UnaryResponseClientCall {
   private let transport: ChannelTransport<RequestPayload, ResponsePayload>
 
   /// The options used to make the RPC.
@@ -152,16 +149,20 @@ public final class ClientStreamingCall<
 }
 
 extension ClientStreamingCall {
-  internal static func makeOnHTTP2Stream(
+  internal static func makeOnHTTP2Stream<Serializer: MessageSerializer, Deserializer: MessageDeserializer>(
     multiplexer: EventLoopFuture<HTTP2StreamMultiplexer>,
+    serializer: Serializer,
+    deserializer: Deserializer,
     callOptions: CallOptions,
     errorDelegate: ClientErrorDelegate?,
     logger: Logger
-  ) -> ClientStreamingCall<RequestPayload, ResponsePayload> {
+  ) -> ClientStreamingCall<RequestPayload, ResponsePayload> where Serializer.Input == RequestPayload, Deserializer.Output == ResponsePayload {
     let eventLoop = multiplexer.eventLoop
     let responsePromise: EventLoopPromise<ResponsePayload> = eventLoop.makePromise()
     let transport = ChannelTransport<RequestPayload, ResponsePayload>(
       multiplexer: multiplexer,
+      serializer: serializer,
+      deserializer: deserializer,
       responseContainer: .init(eventLoop: eventLoop, unaryResponsePromise: responsePromise),
       callType: .clientStreaming,
       timeLimit: callOptions.timeLimit,
@@ -171,11 +172,13 @@ extension ClientStreamingCall {
     return ClientStreamingCall(response: responsePromise.futureResult, transport: transport, options: callOptions)
   }
 
-  internal static func make(
+  internal static func make<Serializer: MessageSerializer, Deserializer: MessageDeserializer>(
+    serializer: Serializer,
+    deserializer: Deserializer,
     fakeResponse: FakeUnaryResponse<RequestPayload, ResponsePayload>?,
     callOptions: CallOptions,
     logger: Logger
-  ) -> ClientStreamingCall<RequestPayload, ResponsePayload> {
+  ) -> ClientStreamingCall<RequestPayload, ResponsePayload> where Serializer.Input == RequestPayload, Deserializer.Output == ResponsePayload {
     let eventLoop = fakeResponse?.channel.eventLoop ?? EmbeddedEventLoop()
     let responsePromise: EventLoopPromise<ResponsePayload> = eventLoop.makePromise()
     let responseContainer = ResponsePartContainer(eventLoop: eventLoop, unaryResponsePromise: responsePromise)

Sources/GRPC/ClientCalls/GRPCClientCallHandler.swift
@@ -16,7 +16,7 @@
 import NIO
 
 /// An inbound channel handler which forwards events and messages to a client call.
-internal class GRPCClientCallHandler<Request: GRPCPayload, Response: GRPCPayload>: ChannelInboundHandler {
+internal class GRPCClientCallHandler<Request, Response>: ChannelInboundHandler {
   typealias InboundIn = _GRPCClientResponsePart<Response>
   private var call: ChannelTransport<Request, Response>
 

commit b0137f96a6c79180e75cb32ab3ce9c5da82fe5d3

Only generate services described in files in the codegen request
Motivation:

Code should only be generated for files listed in the codegen request.
This regressed in #889.

Modifications:

- Drive code generation from files to generate

Result:

Resolves #1057

Sources/protoc-gen-grpc-swift/main.swift
@@ -116,17 +116,20 @@ func main() throws {
   let descriptorSet = DescriptorSet(protos: request.protoFile)
 
   // Only generate output for services.
-  for fileDescriptor in descriptorSet.files where !fileDescriptor.services.isEmpty {
-    let grpcFileName = uniqueOutputFileName(
-      component: "grpc",
-      fileDescriptor: fileDescriptor,
-      fileNamingOption: options.fileNaming
-    )
-    let grpcGenerator = Generator(fileDescriptor, options: options)
-    var grpcFile = Google_Protobuf_Compiler_CodeGeneratorResponse.File()
-    grpcFile.name = grpcFileName
-    grpcFile.content = grpcGenerator.code
-    response.file.append(grpcFile)
+  for name in request.fileToGenerate {
+    let fileDescriptor = descriptorSet.lookupFileDescriptor(protoName: name)
+    if !fileDescriptor.services.isEmpty {
+      let grpcFileName = uniqueOutputFileName(
+        component: "grpc",
+        fileDescriptor: fileDescriptor,
+        fileNamingOption: options.fileNaming
+      )
+      let grpcGenerator = Generator(fileDescriptor, options: options)
+      var grpcFile = Google_Protobuf_Compiler_CodeGeneratorResponse.File()
+      grpcFile.name = grpcFileName
+      grpcFile.content = grpcGenerator.code
+      response.file.append(grpcFile)
+    }
   }
 
   // return everything to the caller