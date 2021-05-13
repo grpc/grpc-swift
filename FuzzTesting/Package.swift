9331f4106e39dd83586f5837fd3e4cb73b676fb9
George Barnett <gbarnett@apple.com>
2021-05-13 14:06:37 +0100

    In-source fuzzing support
    Motivation:
    
    gRPC Swift was recently added to OSS Fuzz. Doing so required patching
    gRPC to make the server pipeline configurator public and a small driver
    program to setup and run a server on an embedded channel. We should make
    supporting oss-fuzz a little nicer!
    
    Modifications:
    
    - Add a FuzzTesting sub package with a 'ServerFuzzer' target
    - Symlink in the echo model and implementation
    - Add an underscore-public API addition to GRPC to configure an
      `EmbeddedChannel` for fuzzing
    
    Result:
    
    Better support for oss-fuzz

---
+++ FuzzTesting/Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+  name: "grpc-swift-fuzzer",
+  dependencies: [
+    .package(name: "grpc-swift", path: ".."),
+    .package(url: "https://github.com/apple/swift-nio.git", from: "2.27.0"),
+  ],
+  targets: [
+    .target(
+      name: "ServerFuzzer",
+      dependencies: [
+        .product(name: "GRPC", package: "grpc-swift"),
+        .product(name: "NIO", package: "swift-nio"),
+        .target(name: "EchoImplementation"),
+      ]
+    ),
+    .target(
+      name: "EchoModel",
+      dependencies: [
+        .product(name: "GRPC", package: "grpc-swift")
+      ]
+    ),
+    .target(
+      name: "EchoImplementation",
+      dependencies: [
+        .product(name: "GRPC", package: "grpc-swift"),
+        .target(name: "EchoModel"),
+      ]
+    )
+  ]
+)
---
+++ FuzzTesting/README.md
@@ -0,0 +1,26 @@
+# gRPC Swift: Fuzz Testing
+
+This package contains binaries for running fuzz testing.
+
+## Building
+
+Building the binary requires additional arguments be passed to the Swift
+compiler:
+
+```
+swift build \
+  -Xswiftc -sanitize=fuzzer,address \
+  -Xswiftc -parse-as-library
+```
+
+Note also that on macOS the Swift toolchain shipped with Xcode _does not_
+currently include fuzzing support and one must use a toolchain
+from [swift.org](https://swift.org/download/). Building on macOS therefore
+requires the above command be run via `xcrun`:
+
+```
+xcrun --toolchain swift \
+  swift build \
+    -Xswiftc -sanitize=fuzzer,address \
+    -Xswiftc -parse-as-library
+```
