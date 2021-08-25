a04ef2a0cf8c6cbfe77dbb81990b22cf5d72d54f
Si Beaumont <beaumont@apple.com>
2021-08-25 20:23:37 +0100

    fixup: remove public from XCTAsyncTest and move to XCTestHelpers
    Signed-off-by: Si Beaumont <beaumont@apple.com>

--- Tests/GRPCTests/XCTestHelpers.swift
+++ Tests/GRPCTests/XCTestHelpers.swift
@@ -696,4 +696,35 @@ func assertThat<Value>(
   }
 }
 
+@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
+extension XCTestCase {
+  /// Cross-platform XCTest support for async-await tests.
+  ///
+  /// Currently the Linux implementation of XCTest doesn't have async-await support.
+  /// Until it does, we make use of this shim which uses a detached `Task` along with
+  /// `XCTest.wait(for:timeout:)` to wrap the operation.
+  ///
+  /// - NOTE: Support for Linux is tracked by https://bugs.swift.org/browse/SR-14403.
+  /// - NOTE: Implementation currently in progress: https://github.com/apple/swift-corelibs-xctest/pull/326
+  func XCTAsyncTest(
+    expectationDescription: String = "Async operation",
+    timeout: TimeInterval = 3,
+    file: StaticString = #file,
+    line: Int = #line,
+    operation: @escaping () async throws -> Void
+  ) {
+    let expectation = self.expectation(description: expectationDescription)
+    Task {
+      do {
+        try await operation()
+      } catch {
+        XCTFail("Error thrown while executing async function @ \(file):\(line): \(error)")
+        Thread.callStackSymbols.forEach { print($0) }
+      }
+      expectation.fulfill()
+    }
+    self.wait(for: [expectation], timeout: timeout)
+  }
+}
+
 #endif
