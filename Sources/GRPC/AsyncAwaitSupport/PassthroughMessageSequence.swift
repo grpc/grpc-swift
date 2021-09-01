17e6382555ccce2a2726759f242538be8962e7bc
George Barnett <gbarnett@apple.com>
2021-09-01 10:26:29 +0100

    Add a passthrough message source and sequence (#1252)
    Motivation:
    
    `AsyncThrowingStream` provides an implementation of `AsyncSequence`
    which allows the holder to provide new values to that sequence from
    within a closure provided to the initializer. This API doesn't fit our
    needs: we must be able to provide the values 'from the outside' rather
    than during initialization.
    
    Modifications:
    
    - Add a `PassthroughMessageSequence`, an implementation of 
      `AsyncSequence` which consumes messages from a
      `PassthroughMessagesSource`.
    - The source  may have values provided to it via `yield(_:)` and terminated
      with `finish()` or `finish(throwing:)`.
    - Add tests and a few `AsyncSequence` helpers.
    
    Result:
    
    We have an `AsyncSequence` implementation which can have values provided
    to it.

---
+++ Sources/GRPC/AsyncAwaitSupport/PassthroughMessageSequence.swift
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2021, gRPC Authors All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#if compiler(>=5.5)
+
+/// An ``AsyncSequence`` adapter for a ``PassthroughMessageSource``.`
+@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
+@usableFromInline
+internal struct PassthroughMessageSequence<Element, Failure: Error>: AsyncSequence {
+  @usableFromInline
+  internal typealias Element = Element
+
+  @usableFromInline
+  internal typealias AsyncIterator = Iterator
+
+  /// The source of messages in the sequence.
+  @usableFromInline
+  internal let _source: PassthroughMessageSource<Element, Failure>
+
+  @usableFromInline
+  internal func makeAsyncIterator() -> Iterator {
+    return Iterator(storage: self._source)
+  }
+
+  @usableFromInline
+  internal init(consuming source: PassthroughMessageSource<Element, Failure>) {
+    self._source = source
+  }
+
+  @usableFromInline
+  internal struct Iterator: AsyncIteratorProtocol {
+    @usableFromInline
+    internal let _storage: PassthroughMessageSource<Element, Failure>
+
+    fileprivate init(storage: PassthroughMessageSource<Element, Failure>) {
+      self._storage = storage
+    }
+
+    @inlinable
+    internal func next() async throws -> Element? {
+      return try await self._storage.consumeNextElement()
+    }
+  }
+}
+
+#endif // compiler(>=5.5)
