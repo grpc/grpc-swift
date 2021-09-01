a14c464ee2a7377eb7ac53e34fc8c860e6b29c4f
George Barnett <gbarnett@apple.com>
2021-09-01 10:26:29 +0100

    Add a passthrough message source and sequence (#1252)
    Motivation:
    
    `AsyncThrowingStream` provides an implementation of `AsyncSequence`
    which allows the holder to provide new values to that sequence from
    within a closure provided to the initializer. This API doesn't fit our
    needs: we must be able to provide the values 'from the outside' rather
    than during initialization.
    
    Modifications:
    
    - Add a `PassthroughMessageSequence`, an implementation of 
      `AsyncSequence` which consumes messages from a
      `PassthroughMessagesSource`.
    - The source  may have values provided to it via `yield(_:)` and terminated
      with `finish()` or `finish(throwing:)`.
    - Add tests and a few `AsyncSequence` helpers.
    
    Result:
    
    We have an `AsyncSequence` implementation which can have values provided
    to it.

--- Tests/GRPCTests/XCTestHelpers.swift
+++ Tests/GRPCTests/XCTestHelpers.swift
@@ -696,36 +696,4 @@ func assertThat<Value>(
   }
 }
 
-@available(macOS 12, iOS 15, tvOS 15, watchOS 8, *)
-extension XCTestCase {
-  /// Cross-platform XCTest support for async-await tests.
-  ///
-  /// Currently the Linux implementation of XCTest doesn't have async-await support.
-  /// Until it does, we make use of this shim which uses a detached `Task` along with
-  /// `XCTest.wait(for:timeout:)` to wrap the operation.
-  ///
-  /// - NOTE: Support for Linux is tracked by https://bugs.swift.org/browse/SR-14403.
-  /// - NOTE: Implementation currently in progress: https://github.com/apple/swift-corelibs-xctest/pull/326
-  func XCTAsyncTest(
-    expectationDescription: String = "Async operation",
-    timeout: TimeInterval = 30,
-    file: StaticString = #filePath,
-    line: UInt = #line,
-    function: StaticString = #function,
-    operation: @escaping () async throws -> Void
-  ) {
-    let expectation = self.expectation(description: expectationDescription)
-    Task {
-      do {
-        try await operation()
-      } catch {
-        XCTFail("Error thrown while executing \(function): \(error)", file: file, line: line)
-        Thread.callStackSymbols.forEach { print($0) }
-      }
-      expectation.fulfill()
-    }
-    self.wait(for: [expectation], timeout: timeout)
-  }
-}
-
 #endif
