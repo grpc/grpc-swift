4813564c2406ab6309fe935b19b10cb69c658b49
George Barnett <gbarnett@apple.com>
2021-09-02 15:33:12 +0100

    Remove unnecessary dependency

--- Sources/GRPC/AsyncAwaitSupport/AsyncWriter.swift
+++ Sources/GRPC/AsyncAwaitSupport/AsyncWriter.swift
@@ -13,9 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 #if compiler(>=5.5)
-import NIOConcurrencyHelpers
 import NIOCore
 
 /// An asynchronous writer which forwards messages to a delegate.
