8189eeeba66f56a2a705a40142ca41d5b6970c8e
George Barnett <gbarnett@apple.com>
2021-09-15 16:56:11 +0100

    Remove unused imports (#1266)
    Motivation:
    
    We import NIOSSL unnecessarily in a few places.
    
    Modifications:
    
    Remove unused imports.
    
    Result:
    
    Fewer unnecessarily imports.

--- Sources/GRPC/TLSVerificationHandler.swift
+++ Sources/GRPC/TLSVerificationHandler.swift
@@ -13,10 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import Foundation
 import Logging
 import NIOCore
-import NIOSSL
 import NIOTLS
 
 /// Application protocol identifiers for ALPN.
