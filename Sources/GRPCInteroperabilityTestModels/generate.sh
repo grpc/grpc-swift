6ab3d0bf81772ebe5833543a11bc36b9ec271884
Si Beaumont <beaumont@apple.com>
2021-11-12 17:47:28 +0000

    Extend interoperability tests to use async provider
    The interoperability tests exercise a well-defined sequence of requests
    for all implementations of gRPC. We already had interop tests for the
    ELG-based client and provider. This pull request extends the tests to
    exercise an async-await–based provider.

--- Sources/GRPCInteroperabilityTestModels/generate.sh
+++ Sources/GRPCInteroperabilityTestModels/generate.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+set -euo pipefail
+
+CURRENT_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 PLUGIN_SWIFT=../../.build/release/protoc-gen-swift
 PLUGIN_SWIFTGRPC=../../.build/release/protoc-gen-grpc-swift
 PROTO="src/proto/grpc/testing/test.proto"
@@ -8,32 +12,32 @@ OUTPUT="Generated"
 FILE_NAMING="DropPath"
 VISIBILITY="Public"
 
-protoc "src/proto/grpc/testing/test.proto" \
+(cd "${CURRENT_SCRIPT_DIR}" && protoc "src/proto/grpc/testing/test.proto" \
   --plugin=${PLUGIN_SWIFT} \
   --plugin=${PLUGIN_SWIFTGRPC} \
   --swift_out=${OUTPUT} \
   --swift_opt=FileNaming=${FILE_NAMING},Visibility=${VISIBILITY} \
   --grpc-swift_out=${OUTPUT} \
-  --grpc-swift_opt=FileNaming=${FILE_NAMING},Visibility=${VISIBILITY}
+  --grpc-swift_opt=FileNaming=${FILE_NAMING},Visibility=${VISIBILITY},ExperimentalAsyncClient=true,ExperimentalAsyncServer=true)
 
-protoc "src/proto/grpc/testing/empty.proto" \
+(cd "${CURRENT_SCRIPT_DIR}" && protoc "src/proto/grpc/testing/empty.proto" \
   --plugin=${PLUGIN_SWIFT} \
   --plugin=${PLUGIN_SWIFTGRPC} \
   --swift_out=${OUTPUT} \
   --swift_opt=FileNaming=${FILE_NAMING},Visibility=${VISIBILITY} \
   --grpc-swift_out=${OUTPUT} \
-  --grpc-swift_opt=FileNaming=${FILE_NAMING},Visibility=${VISIBILITY}
+  --grpc-swift_opt=FileNaming=${FILE_NAMING},Visibility=${VISIBILITY},ExperimentalAsyncClient=true,ExperimentalAsyncServer=true)
 
-protoc "src/proto/grpc/testing/messages.proto" \
+(cd "${CURRENT_SCRIPT_DIR}" && protoc "src/proto/grpc/testing/messages.proto" \
   --plugin=${PLUGIN_SWIFT} \
   --plugin=${PLUGIN_SWIFTGRPC} \
   --swift_out=${OUTPUT} \
   --swift_opt=FileNaming=${FILE_NAMING},Visibility=${VISIBILITY} \
   --grpc-swift_out=${OUTPUT} \
-  --grpc-swift_opt=FileNaming=${FILE_NAMING},Visibility=${VISIBILITY}
+  --grpc-swift_opt=FileNaming=${FILE_NAMING},Visibility=${VISIBILITY},ExperimentalAsyncClient=true,ExperimentalAsyncServer=true)
 
 # The generated code needs to be modified to support testing an unimplemented method.
 # On the server side, the generated code needs to be removed so the server has no
 # knowledge of it. Client code requires no modification, since it is required to call
 # the unimplemented method.
-patch -p3 < unimplemented_call.patch
+(cd "${CURRENT_SCRIPT_DIR}" && patch -p3 < unimplemented_call.patch)
