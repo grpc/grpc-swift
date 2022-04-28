814de4b02db63428fd8feb31502b97190c36fa56
George Barnett <gbarnett@apple.com>
2022-04-28 09:48:12 +0100

    Server handler interceptor state machine
    Motivation:
    
    The existing async server handler has a state machine which deals with
    the interceptor pipeline and the lifecycle of user handler. These are
    really two separate entities each with its own state machine.
    
    This PR is the first in a series which will eventually update the async
    server handler to make use of the two state machines.
    
    Modifications:
    
    - Add a 'ServerInterceptorStateMachine' and tests. This acts as a filter
      for inbound and outbound request and response messages, both as they
      enter and exit the interceptor pipeline. Message parts delievered in the
      wrong order lead to the RPC being cancelled, parts delievered after
      the RPC has finished lead to them being dropped. Cancelling the RPC
      informs the caller to nil out the associated interceptor pipeline.
    - Tests
    - This is not used anywhere (yet).
    
    Result:
    
    Interceptor state machine is in place.

---
+++ Sources/GRPC/AsyncAwaitSupport/AsyncServerHandler/ServerInterceptorStateMachine/ServerInterceptorStateMachine+Actions.swift
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2022, gRPC Authors All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#if compiler(>=5.6)
+extension ServerInterceptorStateMachine {
+  enum InterceptAction: Hashable {
+    /// Forward the message to the interceptor pipeline.
+    case intercept
+    /// Cancel the call.
+    case cancel
+    /// Drop the message.
+    case drop
+
+    init(from streamFilter: ServerInterceptorStateMachine.StreamFilter) {
+      switch streamFilter {
+      case .accept:
+        self = .intercept
+      case .reject:
+        self = .cancel
+      }
+    }
+  }
+
+  enum InterceptedAction: Hashable {
+    /// Forward the message to the network or user handler.
+    case forward
+    /// Cancel the call.
+    case cancel
+    /// Drop the message.
+    case drop
+
+    init(from streamFilter: ServerInterceptorStateMachine.StreamFilter) {
+      switch streamFilter {
+      case .accept:
+        self = .forward
+      case .reject:
+        self = .cancel
+      }
+    }
+  }
+
+  enum CancelAction: Hashable {
+    /// Nil out the interceptor pipeline.
+    case nilOutInterceptorPipeline
+    /// Do nothing.
+    case none
+  }
+}
+#endif // compiler(>=5.6)
---
+++ Sources/GRPC/AsyncAwaitSupport/AsyncServerHandler/ServerInterceptorStateMachine/StreamState.swift
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2022, gRPC Authors All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#if compiler(>=5.6)
+extension ServerInterceptorStateMachine {
+  internal enum StreamFilter: Hashable {
+    case accept
+    case reject
+  }
+
+  internal enum InboundStreamState: Hashable {
+    case idle
+    case receivingMessages
+    case done
+
+    mutating func receiveMetadata() -> StreamFilter {
+      switch self {
+      case .idle:
+        self = .receivingMessages
+        return .accept
+      case .receivingMessages, .done:
+        return .reject
+      }
+    }
+
+    func receiveMessage() -> StreamFilter {
+      switch self {
+      case .receivingMessages:
+        return .accept
+      case .idle, .done:
+        return .reject
+      }
+    }
+
+    mutating func receiveEnd() -> StreamFilter {
+      switch self {
+      case .idle, .receivingMessages:
+        self = .done
+        return .accept
+      case .done:
+        return .reject
+      }
+    }
+  }
+
+  internal enum OutboundStreamState: Hashable {
+    case idle
+    case writingMessages
+    case done
+
+    mutating func sendMetadata() -> StreamFilter {
+      switch self {
+      case .idle:
+        self = .writingMessages
+        return .accept
+      case .writingMessages, .done:
+        return .reject
+      }
+    }
+
+    func sendMessage() -> StreamFilter {
+      switch self {
+      case .writingMessages:
+        return .accept
+      case .idle, .done:
+        return .reject
+      }
+    }
+
+    mutating func sendEnd() -> StreamFilter {
+      switch self {
+      case .idle, .writingMessages:
+        self = .done
+        return .accept
+      case .done:
+        return .reject
+      }
+    }
+  }
+}
+#endif // compiler(>=5.6)
