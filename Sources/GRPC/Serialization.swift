35e46b7cc055bd5c8ae0f43fd17632262f52c586
George Barnett <gbarnett@apple.com>
2022-11-14 10:16:11 +0000

    Update test certs
    Motivation:
    
    Test certs expired; tests now fail.
    
    Modifications:
    
    - Add a script to generate a Swift source file which uses the output of
      the existing `makecert` script.
    - Fix a warning.
    
    Results:
    
    Tests pass.

--- Sources/GRPC/Serialization.swift
+++ Sources/GRPC/Serialization.swift
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import struct Foundation.Data
 import NIOCore
 import NIOFoundationCompat
 import SwiftProtobuf
