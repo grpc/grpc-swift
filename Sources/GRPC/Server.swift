184b4deadf34dd28a8e910e846a0ddf8fc83b3c0
George Barnett <gbarnett@apple.com>
2023-05-03 10:53:47 +0100

    workaround swift versions

--- Sources/GRPC/Server.swift
+++ Sources/GRPC/Server.swift
@@ -550,7 +550,11 @@ extension Server.Configuration.CORS.AllowedOrigins {
   struct AnyCustomCORSAllowedOrigin: GRPCCustomCORSAllowedOrigin {
     private var checkOrigin: @Sendable (String) -> String?
     private let hashInto: @Sendable (inout Hasher) -> Void
+    #if swift(>=5.7)
     private let isEqualTo: @Sendable (any GRPCCustomCORSAllowedOrigin) -> Bool
+    #else
+    private let isEqualTo: @Sendable (Any) -> Bool
+    #endif
 
     init<W: GRPCCustomCORSAllowedOrigin>(_ wrap: W) {
       self.checkOrigin = { wrap.check(origin: $0) }
@@ -566,7 +570,10 @@ extension Server.Configuration.CORS.AllowedOrigins {
       self.hashInto(&hasher)
     }
 
-    static func == (lhs: Self, rhs: Self) -> Bool {
+    static func == (
+      lhs: Server.Configuration.CORS.AllowedOrigins.AnyCustomCORSAllowedOrigin,
+      rhs: Server.Configuration.CORS.AllowedOrigins.AnyCustomCORSAllowedOrigin
+    ) -> Bool {
       return lhs.isEqualTo(rhs)
     }
   }
