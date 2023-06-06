9f4e2e4fd0294fe83372a2b80426a883d5617d2b
George Barnett <gbarnett@apple.com>
2023-06-06 14:40:32 +0100

    Bump version number to 1.17.0 (#1614)
    Motivation:
    
    We plan on tagging a release soon.
    
    Modifications:
    
    - Bump the version to 1.17.0
    
    Result:
    
    The version in the default user-agent string will match the released
    version.

--- Sources/GRPC/Version.swift
+++ Sources/GRPC/Version.swift
@@ -19,7 +19,7 @@ internal enum Version {
   internal static let major = 1
 
   /// The minor version.
-  internal static let minor = 16
+  internal static let minor = 17
 
   /// The patch version.
   internal static let patch = 0
