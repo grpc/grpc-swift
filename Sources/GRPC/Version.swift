bc23f87299f897cf7934e12d50ffef21655a3ae0
Gus Cairo <gus.cairo@apple.com>
2023-07-03 11:13:04 +0100

    Bump version to 1.18.0

--- Sources/GRPC/Version.swift
+++ Sources/GRPC/Version.swift
@@ -19,7 +19,7 @@ internal enum Version {
   internal static let major = 1
 
   /// The minor version.
-  internal static let minor = 17
+  internal static let minor = 18
 
   /// The patch version.
   internal static let patch = 0
