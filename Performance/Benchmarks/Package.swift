99e99561589550c658c8b27f75675acc6435e958
Gustavo Cairo <gus.cairo@apple.com>
2023-10-19 13:34:36 +0100

    Add support for ordo-one/package-benchmark (#1678)

---
+++ Performance/Benchmarks/Package.swift
@@ -0,0 +1,41 @@
+// swift-tools-version: 5.7
+/*
+ * Copyright 2023, gRPC Authors All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import PackageDescription
+
+let package = Package(
+    name: "benchmarks",
+    platforms: [
+        .macOS(.v13),
+    ],
+    dependencies: [
+        .package(path: "../../"),
+        .package(url: "https://github.com/ordo-one/package-benchmark", from: "1.11.2")
+    ],
+    targets: [
+        .executableTarget(
+            name: "GRPCSwiftBenchmark",
+            dependencies: [
+                .product(name: "Benchmark", package: "package-benchmark"),
+                .product(name: "GRPC", package: "grpc-swift")
+            ],
+            path: "Benchmarks/GRPCSwiftBenchmark",
+            plugins: [
+                .plugin(name: "BenchmarkPlugin", package: "package-benchmark")
+            ]
+        ),
+    ]
+)
