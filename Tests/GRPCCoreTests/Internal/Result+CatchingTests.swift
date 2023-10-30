959cbd3f82efd282ebed74f2cc0010d9d702030a
George Barnett <gbarnett@apple.com>
2023-10-30 11:17:43 +0000

    Add a handful of utilities (#1690)
    Motivation:
    
    The client rpc executor makes use of a bunch of utilities. Since it will
    be a reasonably large PR, in order to make it slightly less large, I'd
    like to get some of the utilities reviewed separately. Since most are
    too small to be worth reviewing individually this change includes a
    few unrelated utilities.
    
    Modifications:
    
    - Add `UnsafeTransfer`
    - Adds an optional `cause` error to `RPCError`
    - Add extensions to `Metadata` for setting/parsing a few gRPC specific
      metadata fields
    - Add extensions to `Result` for working with `async` closures and
      casting errors to a known type
    - Add a type-erased closable writer similar to the type-erased writer
    
    Result:
    
    A few handy helpers are in place and the rpc executor PR will be a
    little smaller.

---
+++ Tests/GRPCCoreTests/Internal/Result+CatchingTests.swift
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2023, gRPC Authors All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import XCTest
+
+@testable import GRPCCore
+
+final class ResultCatchingTests: XCTestCase {
+  func testResultCatching() async {
+    let result = await Result {
+      try? await Task.sleep(nanoseconds: 1)
+      throw RPCError(code: .unknown, message: "foo")
+    }
+
+    switch result {
+    case .success:
+      XCTFail()
+    case .failure(let error):
+      XCTAssertEqual(error as? RPCError, RPCError(code: .unknown, message: "foo"))
+    }
+  }
+
+  func testCastToErrorOfCorrectType() async {
+    let result = Result<Void, any Error>.failure(RPCError(code: .unknown, message: "foo"))
+    let typedFailure = result.castError(to: RPCError.self) { _ in
+      XCTFail("buildError(_:) was called")
+      return RPCError(code: .failedPrecondition, message: "shouldn't happen")
+    }
+
+    switch typedFailure {
+    case .success:
+      XCTFail()
+    case .failure(let error):
+      XCTAssertEqual(error, RPCError(code: .unknown, message: "foo"))
+    }
+  }
+
+  func testCastToErrorOfIncorrectType() async {
+    struct WrongError: Error {}
+    let result = Result<Void, any Error>.failure(WrongError())
+    let typedFailure = result.castError(to: RPCError.self) { _ in
+      return RPCError(code: .invalidArgument, message: "fallback")
+    }
+
+    switch typedFailure {
+    case .success:
+      XCTFail()
+    case .failure(let error):
+      XCTAssertEqual(error, RPCError(code: .invalidArgument, message: "fallback"))
+    }
+  }
+}
