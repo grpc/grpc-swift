c841cbb6acb8dbb118325088976c070d068e5e5c
Rick Newton-Rogers <rnewtonrogers@apple.com>
2023-11-01 10:57:06 +0000

    guard testPreviousRPCAttemptsValidValues (#1698)
    It relies on `metadata.previousRPCAttempts`, `retryPushback` which are only available on
    macOS 13.0 and newer.

--- Tests/GRPCCoreTests/Internal/Metadata+GRPCTests.swift
+++ Tests/GRPCCoreTests/Internal/Metadata+GRPCTests.swift
@@ -18,6 +18,7 @@ import XCTest
 
 @testable import GRPCCore
 
+@available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *)
 final class MetadataGRPCTests: XCTestCase {
   func testPreviousRPCAttemptsValidValues() {
     let testData = [("0", 0), ("1", 1), ("-1", -1)]
