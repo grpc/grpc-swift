1fea48d3a5242553b8acbf0ade122f1d2adaec10
Stefana Dranca <sdranca@apple.com>
2023-11-01 13:35:52 +0000

    added OS versions availability check

--- Sources/GRPCReflectionService/Server/ReflectionService.swift
+++ Sources/GRPCReflectionService/Server/ReflectionService.swift
@@ -213,7 +213,7 @@ internal final class ReflectionServiceProvider: Grpc_Reflection_V1_ServerReflect
 
   internal init(protoFilePaths: [String]) throws {
     let binaryFilesURLs = protoFilePaths.map {
-      if #available(macOS 13.0, *) {
+      if #available(macOS 13.0, iOS 16.0, tvOS 16.0, watchOS 9.0, *) {
         URL(filePath: $0)
       } else {
         URL(fileURLWithPath: $0)
