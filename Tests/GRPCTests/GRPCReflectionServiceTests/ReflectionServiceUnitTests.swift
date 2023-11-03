55196026c0afe089a35a60f0253ab30a709a89a0
Stefana-Ioana Dranca <66513820+stefanadranca@users.noreply.github.com>
2023-11-03 10:53:52 +0000

    Added ReflectionService initialiser using file paths (#1699)
    Motivation:
    
    Initializing the Reflection Service using file descriptor protos is too laborious for users,
    so simply passing the file paths of the binary files containing the serialized file descriptor protos
    and letting the Service extract the data from those paths is more convenient.
    
    Modifications:
    
    Added a new initializer that takes in file paths and extracts and converts to file descriptor proto format the Data
    from the files. Also modified the integration tests to create temporary files containing the serialized file descriptor protos.
    
    Result:
    
    GRPCUsers can add the ReflectionService to their Server in a more convenient way.

--- Tests/GRPCTests/GRPCReflectionServiceTests/ReflectionServiceUnitTests.swift
+++ Tests/GRPCTests/GRPCReflectionServiceTests/ReflectionServiceUnitTests.swift
@@ -519,4 +519,80 @@ final class ReflectionServiceUnitTests: GRPCTestCase {
 
     XCTAssertEqual(try extensionsFieldNumbersOfTypeResult.get(), [1, 2, 3, 4, 5, 130])
   }
+
+  func testReadSerializedFileDescriptorProto() throws {
+    let initialFileDescriptorProto = generateFileDescriptorProto(fileName: "test", suffix: "1")
+    let data = try initialFileDescriptorProto.serializedData().base64EncodedData()
+    #if os(Linux)
+    let temporaryDirectory = "/tmp/"
+    #else
+    let temporaryDirectory = FileManager.default.temporaryDirectory.path()
+    #endif
+    let filePath = "\(temporaryDirectory)test\(UUID()).grpc.reflection"
+    FileManager.default.createFile(atPath: filePath, contents: data)
+    defer {
+      XCTAssertNoThrow(try FileManager.default.removeItem(atPath: filePath))
+    }
+    let reflectionServiceFileDescriptorProto =
+      try ReflectionService.readSerializedFileDescriptorProto(atPath: filePath)
+    XCTAssertEqual(reflectionServiceFileDescriptorProto, initialFileDescriptorProto)
+  }
+
+  func testReadSerializedFileDescriptorProtoInvalidFileContents() throws {
+    let invalidData = "%%%%%££££".data(using: .utf8)
+    #if os(Linux)
+    let temporaryDirectory = "/tmp/"
+    #else
+    let temporaryDirectory = FileManager.default.temporaryDirectory.path()
+    #endif
+    let filePath = "\(temporaryDirectory)test\(UUID()).grpc.reflection"
+    FileManager.default.createFile(atPath: filePath, contents: invalidData)
+    defer {
+      XCTAssertNoThrow(try FileManager.default.removeItem(atPath: filePath))
+    }
+
+    XCTAssertThrowsGRPCStatus(
+      try ReflectionService.readSerializedFileDescriptorProto(atPath: filePath)
+    ) {
+      status in
+      XCTAssertEqual(
+        status,
+        GRPCStatus(
+          code: .invalidArgument,
+          message:
+            """
+            The \(filePath) file contents could not be transformed \
+            into serialized data representing a file descriptor proto.
+            """
+        )
+      )
+    }
+  }
+
+  func testReadSerializedFileDescriptorProtos() throws {
+    let initialFileDescriptorProtos = makeProtosWithDependencies()
+    var filePaths: [String] = []
+
+    for initialFileDescriptorProto in initialFileDescriptorProtos {
+      let data = try initialFileDescriptorProto.serializedData()
+        .base64EncodedData()
+      #if os(Linux)
+      let temporaryDirectory = "/tmp/"
+      #else
+      let temporaryDirectory = FileManager.default.temporaryDirectory.path()
+      #endif
+      let filePath = "\(temporaryDirectory)test\(UUID()).grpc.reflection"
+      FileManager.default.createFile(atPath: filePath, contents: data)
+      filePaths.append(filePath)
+    }
+    defer {
+      for filePath in filePaths {
+        XCTAssertNoThrow(try FileManager.default.removeItem(atPath: filePath))
+      }
+    }
+
+    let reflectionServiceFileDescriptorProtos =
+      try ReflectionService.readSerializedFileDescriptorProtos(atPaths: filePaths)
+    XCTAssertEqual(reflectionServiceFileDescriptorProtos, initialFileDescriptorProtos)
+  }
 }
