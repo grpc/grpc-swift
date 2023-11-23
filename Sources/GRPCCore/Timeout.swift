5dbf942c968fe189066630fa2e0f3d3fd426c66b
Gus Cairo <gus.cairo@apple.com>
2023-11-23 13:34:59 +0000

    Add Timeout type to GRPCCore

---
+++ Sources/GRPCCore/Timeout.swift
@@ -0,0 +1,153 @@
+/*
+ * Copyright 2023, gRPC Authors All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import Dispatch
+
+/// A timeout for a gRPC call.
+///
+/// Timeouts must be positive and at most 8-digits long.
+/// See "Timeout" in https://github.com/grpc/grpc/blob/master/doc/PROTOCOL-HTTP2.md#requests
+@available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *)
+public struct Timeout: CustomStringConvertible, Equatable {
+  /// The largest amount of any unit of time which may be represented by a gRPC timeout.
+  internal static let maxAmount: Int64 = 99_999_999
+
+  /// The wire encoding of this timeout as described in the gRPC protocol.
+  /// See: https://github.com/grpc/grpc/blob/master/doc/PROTOCOL-HTTP2.md.
+  public let wireEncoding: String
+  public let duration: Duration
+
+  public var description: String {
+    return self.wireEncoding
+  }
+  
+  public init?(stringLiteral value: String) {
+    guard 2...8 ~= value.count else {
+      return nil
+    }
+
+    if let amount = Int64(value.dropLast()), 
+       let unit = TimeoutUnit(rawValue: value.last!) {
+      self = Self.init(amount: amount, unit: unit)
+    } else {
+      return nil
+    }
+  }
+
+  /// Creates a `GRPCTimeout`.
+  ///
+  /// - Precondition: The amount should be greater than or equal to zero and less than or equal
+  ///   to `GRPCTimeout.maxAmount`.
+  internal init(amount: Int64, unit: TimeoutUnit) {
+    precondition(0...Timeout.maxAmount ~= amount)
+    
+    self.duration = Duration(amount: amount, unit: unit)
+    self.wireEncoding = "\(amount)\(unit.rawValue)"
+  }
+
+  /// Create a timeout by rounding up the timeout so that it may be represented in the gRPC
+  /// wire format.
+  internal init(rounding amount: Int64, unit: TimeoutUnit) {
+    var roundedAmount = amount
+    var roundedUnit = unit
+
+    if roundedAmount <= 0 {
+      roundedAmount = 0
+    } else {
+      while roundedAmount > Timeout.maxAmount {
+        switch roundedUnit {
+        case .nanoseconds:
+          roundedAmount = roundedAmount.quotientRoundedUp(dividingBy: 1000)
+          roundedUnit = .microseconds
+        case .microseconds:
+          roundedAmount = roundedAmount.quotientRoundedUp(dividingBy: 1000)
+          roundedUnit = .milliseconds
+        case .milliseconds:
+          roundedAmount = roundedAmount.quotientRoundedUp(dividingBy: 1000)
+          roundedUnit = .seconds
+        case .seconds:
+          roundedAmount = roundedAmount.quotientRoundedUp(dividingBy: 60)
+          roundedUnit = .minutes
+        case .minutes:
+          roundedAmount = roundedAmount.quotientRoundedUp(dividingBy: 60)
+          roundedUnit = .hours
+        case .hours:
+          roundedAmount = Timeout.maxAmount
+          roundedUnit = .hours
+        }
+      }
+    }
+
+    self.init(amount: roundedAmount, unit: roundedUnit)
+  }
+}
+
+extension Int64 {
+  /// Returns the quotient of this value when divided by `divisor` rounded up to the nearest
+  /// multiple of `divisor` if the remainder is non-zero.
+  ///
+  /// - Parameter divisor: The value to divide this value by.
+  fileprivate func quotientRoundedUp(dividingBy divisor: Int64) -> Int64 {
+    let (quotient, remainder) = self.quotientAndRemainder(dividingBy: divisor)
+    return quotient + (remainder != 0 ? 1 : 0)
+  }
+}
+
+@available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *)
+extension Duration {
+  /// Construct a `Duration` given a number of minutes represented as an `Int64`.
+  ///
+  ///       let d: Duration = .minutes(5)
+  ///
+  /// - Returns: A `Duration` representing a given number of minutes.
+  internal static func minutes(_ minutes: Int64) -> Duration {
+    return Self.init(secondsComponent: 60 * minutes, attosecondsComponent: 0)
+  }
+  
+  /// Construct a `Duration` given a number of hours represented as an `Int64`.
+  ///
+  ///       let d: Duration = .hours(3)
+  ///
+  /// - Returns: A `Duration` representing a given number of hours.
+  internal static func hours(_ hours: Int64) -> Duration {
+    return Self.init(secondsComponent: 60 * 60 * hours, attosecondsComponent: 0)
+  }
+  
+  internal init(amount: Int64, unit: TimeoutUnit) {
+    switch unit {
+    case .hours:
+      self = Self.hours(amount)
+    case .minutes:
+      self = Self.minutes(amount)
+    case .seconds:
+      self = Self.seconds(amount)
+    case .milliseconds:
+      self = Self.milliseconds(amount)
+    case .microseconds:
+      self = Self.microseconds(amount)
+    case .nanoseconds:
+      self = Self.nanoseconds(amount)
+    }
+  }
+}
+
+internal enum TimeoutUnit: Character {
+  case hours = "H"
+  case minutes = "M"
+  case seconds = "S"
+  case milliseconds = "m"
+  case microseconds = "u"
+  case nanoseconds = "n"
+}
---
+++ Tests/GRPCCoreTests/TimeoutTests.swift
@@ -0,0 +1,163 @@
+/*
+ * Copyright 2023, gRPC Authors All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+@testable import GRPCCore
+import XCTest
+
+final class TimeoutTests: XCTestCase {
+  func testDecodeInvalidTimeout_Empty() {
+    let timeoutHeader = ""
+    XCTAssertNil(Timeout(stringLiteral: timeoutHeader))
+  }
+  
+  func testDecodeInvalidTimeout_NoAmount() {
+    let timeoutHeader = "H"
+    XCTAssertNil(Timeout(stringLiteral: timeoutHeader))
+  }
+  
+  func testDecodeInvalidTimeout_NoUnit() {
+    let timeoutHeader = "123"
+    XCTAssertNil(Timeout(stringLiteral: timeoutHeader))
+  }
+  
+  func testDecodeInvalidTimeout_TooLongAmount() {
+    let timeoutHeader = "100000000S"
+    XCTAssertNil(Timeout(stringLiteral: timeoutHeader))
+  }
+  
+  func testDecodeInvalidTimeout_InvalidUnit() {
+    let timeoutHeader = "123j"
+    XCTAssertNil(Timeout(stringLiteral: timeoutHeader))
+  }
+  
+  func testDecodeValidTimeout_Hours(){
+    let timeoutHeader = "123H"
+    let timeout = Timeout(stringLiteral: timeoutHeader)
+    XCTAssertNotNil(timeout)
+    XCTAssertEqual(timeout!.duration, Duration.hours(123))
+    XCTAssertEqual(timeout!.wireEncoding, timeoutHeader)
+  }
+  
+  func testDecodeValidTimeout_Minutes(){
+    let timeoutHeader = "123M"
+    let timeout = Timeout(stringLiteral: timeoutHeader)
+    XCTAssertNotNil(timeout)
+    XCTAssertEqual(timeout!.duration, Duration.minutes(123))
+    XCTAssertEqual(timeout!.wireEncoding, timeoutHeader)
+  }
+  
+  func testDecodeValidTimeout_Seconds(){
+    let timeoutHeader = "123S"
+    let timeout = Timeout(stringLiteral: timeoutHeader)
+    XCTAssertNotNil(timeout)
+    XCTAssertEqual(timeout!.duration, Duration.seconds(123))
+    XCTAssertEqual(timeout!.wireEncoding, timeoutHeader)
+  }
+  
+  func testDecodeValidTimeout_Milliseconds(){
+    let timeoutHeader = "123m"
+    let timeout = Timeout(stringLiteral: timeoutHeader)
+    XCTAssertNotNil(timeout)
+    XCTAssertEqual(timeout!.duration, Duration.milliseconds(123))
+    XCTAssertEqual(timeout!.wireEncoding, timeoutHeader)
+  }
+  
+  func testDecodeValidTimeout_Microseconds(){
+    let timeoutHeader = "123u"
+    let timeout = Timeout(stringLiteral: timeoutHeader)
+    XCTAssertNotNil(timeout)
+    XCTAssertEqual(timeout!.duration, Duration.microseconds(123))
+    XCTAssertEqual(timeout!.wireEncoding, timeoutHeader)
+  }
+  
+  func testDecodeValidTimeout_Nanoseconds(){
+    let timeoutHeader = "123n"
+    let timeout = Timeout(stringLiteral: timeoutHeader)
+    XCTAssertNotNil(timeout)
+    XCTAssertEqual(timeout!.duration, Duration.nanoseconds(123))
+    XCTAssertEqual(timeout!.wireEncoding, timeoutHeader)
+  }
+  
+  func testRoundingNegativeTimeout() {
+    let timeout = Timeout(rounding: -10, unit: .seconds)
+    XCTAssertEqual(String(describing: timeout), "0S")
+    XCTAssertEqual(timeout.duration, .seconds(0))
+  }
+
+  func testRoundingNanosecondsTimeout() throws {
+    let timeout = Timeout(rounding: 123_456_789, unit: .nanoseconds)
+    XCTAssertEqual(timeout, Timeout(amount: 123_457, unit: .microseconds))
+
+    // 123_456_789 (nanoseconds) / 1_000
+    //   = 123_456.789
+    //   = 123_457 (microseconds, rounded up)
+    XCTAssertEqual(String(describing: timeout), "123457u")
+    XCTAssertEqual(timeout.duration, .microseconds(123_457))
+  }
+
+  func testRoundingMicrosecondsTimeout() throws {
+    let timeout = Timeout(rounding: 123_456_789, unit: .microseconds)
+    XCTAssertEqual(timeout, Timeout(amount: 123_457, unit: .milliseconds))
+
+    // 123_456_789 (microseconds) / 1_000
+    //   = 123_456.789
+    //   = 123_457 (milliseconds, rounded up)
+    XCTAssertEqual(String(describing: timeout), "123457m")
+    XCTAssertEqual(timeout.duration, .milliseconds(123_457))
+  }
+
+  func testRoundingMillisecondsTimeout() throws {
+    let timeout = Timeout(rounding: 123_456_789, unit: .milliseconds)
+    XCTAssertEqual(timeout, Timeout(amount: 123_457, unit: .seconds))
+
+    // 123_456_789 (milliseconds) / 1_000
+    //   = 123_456.789
+    //   = 123_457 (seconds, rounded up)
+    XCTAssertEqual(String(describing: timeout), "123457S")
+    XCTAssertEqual(timeout.duration, .seconds(123_457))
+  }
+
+  func testRoundingSecondsTimeout() throws {
+    let timeout = Timeout(rounding: 123_456_789, unit: .seconds)
+    XCTAssertEqual(timeout, Timeout(amount: 2_057_614, unit: .minutes))
+
+    // 123_456_789 (seconds) / 60
+    //   = 2_057_613.15
+    //   = 2_057_614 (minutes, rounded up)
+    XCTAssertEqual(String(describing: timeout), "2057614M")
+    XCTAssertEqual(timeout.duration, .minutes(2057614))
+  }
+
+  func testRoundingMinutesTimeout() throws {
+    let timeout = Timeout(rounding: 123_456_789, unit: .minutes)
+    XCTAssertEqual(timeout, Timeout(amount: 2_057_614, unit: .hours))
+
+    // 123_456_789 (minutes) / 60
+    //   = 2_057_613.15
+    //   = 2_057_614 (hours, rounded up)
+    XCTAssertEqual(String(describing: timeout), "2057614H")
+    XCTAssertEqual(timeout.duration, .hours(2057614))
+  }
+
+  func testRoundingHoursTimeout() throws {
+    let timeout = Timeout(rounding: 123_456_789, unit: .hours)
+    XCTAssertEqual(timeout, Timeout(amount: 99_999_999, unit: .hours))
+
+    // Hours are the largest unit of time we have (as per the gRPC spec) so we can't round to a
+    // different unit. In this case we clamp to the largest value.
+    XCTAssertEqual(String(describing: timeout), "99999999H")
+    XCTAssertEqual(timeout.duration, .hours(Timeout.maxAmount))
+  }
+}
