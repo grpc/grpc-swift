130cef2101a2ce869dfa776100b76809e1dc3b0c
Stefana Dranca <sdranca@apple.com>
2023-11-29 10:27:19 +0200

    replaced Component type with String in TypeName

--- Sources/GRPCCodeGen/Internal/TypeName.swift
+++ Sources/GRPCCodeGen/Internal/TypeName.swift
@@ -34,45 +34,26 @@ import Foundation
 /// Use the type name to define a type, see also `TypeUsage` when referring
 /// to a type.
 struct TypeName: Hashable {
-
-  /// Describes a single component of the Swift path.
-  struct Component: Hashable {
-    /// The name of the Swift path component.
-    var swift: String?
-  }
-
   /// A list of components that make up the type name.
-  private let components: [Component]
-
-  /// The list of Swift path components.
-  var swiftKeyPathComponents: [String] { components.compactMap(\.swift) }
+  private let components: [String]
 
   /// Creates a new type name with the specified list of components.
   /// - Parameter components: A list of components for the type.
-  init(components: [Component]) {
-    precondition(!components.compactMap(\.swift).isEmpty, "TypeName Swift key path cannot be empty")
+  init(components: [String]) {
+    precondition(!components.isEmpty, "TypeName path cannot be empty")
     self.components = components
-  }
-
-  /// Creates a new type name with the specified list of Swift path
-  /// components.
-  ///
-  /// - Parameter swiftKeyPath: A list of Swift path components for the type.
-  init(swiftKeyPath: [String]) {
-    precondition(!swiftKeyPath.isEmpty, "TypeName Swift key path cannot be empty")
-    self.init(components: swiftKeyPath.map { .init(swift: $0) })
   }
 
   /// A string representation of the fully qualified Swift type name.
   ///
   /// For example: `Swift.Int`.
-  var fullyQualifiedSwiftName: String { swiftKeyPathComponents.joined(separator: ".") }
+  var fullyQualifiedName: String { components.joined(separator: ".") }
 
   /// A string representation of the last path component of the Swift
   /// type name.
   ///
   /// For example: `Int`.
-  var shortSwiftName: String { swiftKeyPathComponents.last! }
+  var shortName: String { components.last! }
 
   /// Returns a type name by appending the specified components to the
   /// current type name.
@@ -80,12 +61,11 @@ struct TypeName: Hashable {
   /// In other words, returns a type name for a child type.
   /// - Precondition: At least one of the components must be non-nil.
   /// - Parameters:
-  ///   - swiftComponent: The name of the Swift type component.
+  ///   - component: The name of the Swift type component.
   /// - Returns: A new type name.
-  func appending(swiftComponent: String? = nil) -> Self {
-    precondition(swiftComponent != nil, "The Swift name must be non-nil.")
-    let newComponent = Component(swift: swiftComponent)
-    return .init(components: components + [newComponent])
+  func appending(component: String? = nil) -> Self {
+    precondition(component != nil, "The type name must be non-nil.")
+    return .init(components: components + [component])
   }
 
   /// Returns a type name by removing the last component from the current
@@ -115,13 +95,13 @@ extension TypeName {
   /// Swift module.
   /// - Parameter name: The name of the type.
   /// - Returns: A TypeName representing the specified type within the Swift module.
-  static func swift(_ name: String) -> TypeName { TypeName(swiftKeyPath: ["Swift", name]) }
+  static func swift(_ name: String) -> TypeName { TypeName(components: ["Swift", name]) }
 
   /// Returns a type name for a type with the specified name in the
   /// Foundation module.
   /// - Parameter name: The name of the type.
   /// - Returns: A TypeName representing the specified type within the Foundation module.
   static func foundation(_ name: String) -> TypeName {
-    TypeName(swiftKeyPath: ["Foundation", name])
+    TypeName(components: ["Foundation", name])
   }
 }
