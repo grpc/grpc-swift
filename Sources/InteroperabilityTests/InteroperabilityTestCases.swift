213a53db9ea990ac509556bdca3500a683783819
Stefana Dranca <sdranca@apple.com>
2024-03-05 16:10:26 +0000

    fixed header

--- Sources/InteroperabilityTests/InteroperabilityTestCases.swift
+++ Sources/InteroperabilityTests/InteroperabilityTestCases.swift
@@ -1,6 +1,6 @@
 import Dispatch
 /*
- * Copyright 2019, gRPC Authors All rights reserved.
+ * Copyright 2024, gRPC Authors All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
