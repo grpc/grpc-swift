26172a8fc3919fe951339052d2d5dbcad9be1271
Stefana Dranca <sdranca@apple.com>
2024-03-19 15:04:02 +0000

    implemented feedback

--- Sources/performance-worker/WorkerService.swift
+++ Sources/performance-worker/WorkerService.swift
@@ -20,14 +20,14 @@ import NIOCore
 
 @available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *)
 final class WorkerService: Grpc_Testing_WorkerService.ServiceProtocol, Sendable {
-  let state: NIOLockedValueBox<State>
+  private let state: NIOLockedValueBox<State>
 
   init() {
     let clientAndServer = State()
     self.state = NIOLockedValueBox(clientAndServer)
   }
 
-  struct State {
+  private struct State {
     var role: Role?
 
     enum Role {
@@ -54,16 +54,20 @@ final class WorkerService: Grpc_Testing_WorkerService.ServiceProtocol, Sendable 
     request: ServerRequest.Single<Grpc_Testing_WorkerService.Method.QuitWorker.Input>
   ) async throws -> ServerResponse.Single<Grpc_Testing_WorkerService.Method.QuitWorker.Output> {
 
-    if let role = self.state.withLockedValue({ $0.role }) {
+    let role = self.state.withLockedValue { state in
+      defer { state.role = nil }
+      return state.role
+    }
+
+    if let role = role {
       switch role {
       case .client(let client):
         client.close()
-        self.state.withLockedValue({ $0.role = nil })
       case .server(let server):
         server.stopListening()
-        self.state.withLockedValue({ $0.role = nil })
       }
     }
+
     return ServerResponse.Single(message: Grpc_Testing_WorkerService.Method.QuitWorker.Output())
   }
 
