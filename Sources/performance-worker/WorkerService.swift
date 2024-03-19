d2065f7a6f089b9351fdd5bb3dabf8519bce3f50
Stefana Dranca <sdranca@apple.com>
2024-03-19 11:26:15 +0000

    added state to worker service and made it sendable

--- Sources/performance-worker/WorkerService.swift
+++ Sources/performance-worker/WorkerService.swift
@@ -15,20 +15,54 @@
  */
 
 import GRPCCore
+import NIOConcurrencyHelpers
 import NIOCore
 
 @available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *)
-struct WorkerService: Grpc_Testing_WorkerService.ServiceProtocol {
-  var testClient: GRPCClient? = nil
-  var testServer: GRPCServer? = nil
+final class WorkerService: Grpc_Testing_WorkerService.ServiceProtocol, Sendable {
+  let state: NIOLockedValueBox<State>
+
+  init() {
+    let clientAndServer = State()
+    self.state = NIOLockedValueBox(clientAndServer)
+  }
+
+  struct State {
+    var role: Role?
+
+    enum Role {
+      case client(GRPCClient)
+      case server(GRPCServer)
+    }
+
+    init() {}
+
+    init(role: Role) {
+      self.role = role
+    }
+
+    init(server: GRPCServer) {
+      self.role = .server(server)
+    }
+
+    init(client: GRPCClient) {
+      self.role = .client(client)
+    }
+  }
 
   func quitWorker(
     request: ServerRequest.Single<Grpc_Testing_WorkerService.Method.QuitWorker.Input>
   ) async throws -> ServerResponse.Single<Grpc_Testing_WorkerService.Method.QuitWorker.Output> {
-    if let testClient = self.testClient {
-      testClient.close()
-    } else if let testServer = self.testServer {
-      testServer.stopListening()
+
+    if let role = self.state.withLockedValue({ $0.role }) {
+      switch role {
+      case .client(let client):
+        client.close()
+        self.state.withLockedValue({ $0.role = nil })
+      case .server(let server):
+        server.stopListening()
+        self.state.withLockedValue({ $0.role = nil })
+      }
     }
     return ServerResponse.Single(message: Grpc_Testing_WorkerService.Method.QuitWorker.Output())
   }
