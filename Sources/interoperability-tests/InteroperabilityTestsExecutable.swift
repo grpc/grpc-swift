fd0f881842793981455bf1b7aff7e1c0b2c6b1b9
Gustavo Cairo <gus.cairo@apple.com>
2024-05-31 09:31:36 +0100

    Add server executable for interop tests (#1897)

---
+++ Sources/interoperability-tests/InteroperabilityTestsExecutable.swift
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2024, gRPC Authors All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import ArgumentParser
+import GRPCCore
+import GRPCHTTP2Core
+import GRPCHTTP2TransportNIOPosix
+import InteroperabilityTests
+import NIOPosix
+
+@main
+@available(macOS 14.0, iOS 17.0, watchOS 10.0, tvOS 17.0, *)
+struct InteroperabilityTestsExecutable: AsyncParsableCommand {
+  static var configuration = CommandConfiguration(
+    abstract: "gRPC Swift Interoperability Runner",
+    subcommands: [StartServer.self, ListTests.self]
+  )
+
+  struct StartServer: AsyncParsableCommand {
+    static var configuration = CommandConfiguration(
+      abstract: "Start the gRPC Swift interoperability test server."
+    )
+
+    @Option(help: "The port to listen on for new connections")
+    var port: Int
+
+    func run() async throws {
+      var transportConfig = HTTP2ServerTransport.Posix.Config.defaults
+      transportConfig.compression.enabledAlgorithms = .all
+      let transport = HTTP2ServerTransport.Posix(
+        address: .ipv4(host: "0.0.0.0", port: self.port),
+        config: transportConfig
+      )
+      let server = GRPCServer(transport: transport, services: [TestService()])
+      try await server.run()
+    }
+  }
+
+  struct ListTests: ParsableCommand {
+    static var configuration = CommandConfiguration(
+      abstract: "List all interoperability test names."
+    )
+
+    func run() throws {
+      for testCase in InteroperabilityTestCase.allCases {
+        print(testCase.name)
+      }
+    }
+  }
+}
