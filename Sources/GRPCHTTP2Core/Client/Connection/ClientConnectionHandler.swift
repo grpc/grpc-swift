bd4354dc47b57a9b8ec5c49f58efc32574773f69
George Barnett <gbarnett@apple.com>
2024-06-04 15:47:53 +0100

    Add comment about lack of goaway

--- Sources/GRPCHTTP2Core/Client/Connection/ClientConnectionHandler.swift
+++ Sources/GRPCHTTP2Core/Client/Connection/ClientConnectionHandler.swift
@@ -247,6 +247,8 @@ final class ClientConnectionHandler: ChannelInboundHandler, ChannelOutboundHandl
         switch self.state.beginGracefulShutdown(promise: promise) {
         case .sendGoAway(let close):
           context.fireChannelRead(self.wrapInboundOut(.closing(.initiatedLocally)))
+          // The client could send a GOAWAY at this point but it's not really necessary, the server
+          // can't open streams anyway, the client will just close the connection when it's done.
           if close {
             context.close(promise: nil)
           }
