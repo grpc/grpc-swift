1b528a471935e1c322403749c5f1005b5699d5db
George Barnett <gbarnett@apple.com>
2024-06-05 09:07:29 +0100

    Add a NIOPosix based client transport
    Motivation:
    
    We have all the pieces in place for a client transport, we can now
    create one.
    
    Modifications:
    
    - Add a NIOPosix based client transport
    
    Result:
    
    Can create a NIOPosix based client transport

---
+++ Sources/GRPCHTTP2TransportNIOPosix/HTTP2ClientTransport+Posix.swift
@@ -0,0 +1,155 @@
+/*
+ * Copyright 2024, gRPC Authors All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import GRPCCore
+@_spi(Package) import GRPCHTTP2Core
+import NIOCore
+import NIOPosix
+
+@available(macOS 14.0, iOS 17.0, watchOS 10.0, tvOS 17.0, *)
+extension HTTP2ClientTransport {
+  public struct Posix: ClientTransport {
+    private let channel: GRPCChannel
+
+    public init(
+      target: any ResolvableTarget,
+      resolverRegistry: NameResolverRegistry = .defaults,
+      config: Config.Posix = .defaults,
+      serviceConfig: ServiceConfig = ServiceConfig()
+    ) throws {
+      guard let resolver = resolverRegistry.makeResolver(for: target) else {
+        throw RuntimeError(
+          code: .transportError,
+          message: """
+            No suitable resolvers to resolve '\(target)'. You must make sure that the resolver \
+            registry has a suitable name resolve factory registered for the given target.
+            """
+        )
+      }
+
+      // Configure a connector.
+      self.channel = GRPCChannel(
+        resolver: resolver,
+        connector: Connector(eventLoopGroup: .singletonMultiThreadedEventLoopGroup, config: config),
+        config: GRPCChannel.Config(posix: config),
+        defaultServiceConfig: serviceConfig
+      )
+    }
+
+    public var retryThrottle: RetryThrottle? {
+      self.channel.retryThrottle
+    }
+
+    public func connect() async {
+      await self.channel.connect()
+    }
+
+    public func configuration(forMethod descriptor: MethodDescriptor) -> MethodConfig? {
+      self.channel.configuration(forMethod: descriptor)
+    }
+
+    public func close() {
+      self.channel.close()
+    }
+
+    public func withStream<T>(
+      descriptor: MethodDescriptor,
+      options: CallOptions,
+      _ closure: (RPCStream<Inbound, Outbound>) async throws -> T
+    ) async throws -> T {
+      try await self.channel.withStream(descriptor: descriptor, options: options, closure)
+    }
+  }
+}
+
+@available(macOS 14.0, iOS 17.0, watchOS 10.0, tvOS 17.0, *)
+extension HTTP2ClientTransport.Posix {
+  struct Connector: HTTP2Connector {
+    private let config: HTTP2ClientTransport.Config.Posix
+    private let eventLoopGroup: any EventLoopGroup
+
+    init(eventLoopGroup: any EventLoopGroup, config: HTTP2ClientTransport.Config.Posix) {
+      self.eventLoopGroup = eventLoopGroup
+      self.config = config
+    }
+
+    func establishConnection(
+      to address: GRPCHTTP2Core.SocketAddress
+    ) async throws -> HTTP2Connection {
+      let (channel, multiplexer) = try await ClientBootstrap(
+        group: self.eventLoopGroup
+      ).connect(to: address) { channel in
+        channel.eventLoop.makeCompletedFuture {
+          try channel.pipeline.syncOperations.configureGRPCClientPipeline(
+            channel: channel,
+            config: GRPCChannel.Config(posix: self.config)
+          )
+        }
+      }
+
+      return HTTP2Connection(channel: channel, multiplexer: multiplexer, isPlaintext: false)
+    }
+  }
+}
+
+extension HTTP2ClientTransport.Config {
+  @available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *)
+  public struct Posix: Sendable {
+    /// Configuration for HTTP/2 connections.
+    public var http2: HTTP2
+
+    /// Configuration for backoff used when establishing a connection.
+    public var backoff: Backoff
+
+    /// Configuration for connection management.
+    public var connection: Connection
+
+    /// Compression configuration.
+    public var compression: Compression
+
+    /// Creates a new connection configuration.
+    ///
+    /// See also ``defaults``.
+    public init(http2: HTTP2, backoff: Backoff, connection: Connection, compression: Compression) {
+      self.http2 = http2
+      self.connection = connection
+      self.backoff = backoff
+      self.compression = compression
+    }
+
+    /// Default values.
+    public static var defaults: Self {
+      Posix(
+        http2: .defaults,
+        backoff: .defaults,
+        connection: .defaults,
+        compression: .defaults
+      )
+    }
+  }
+}
+
+@available(macOS 14.0, iOS 17.0, watchOS 10.0, tvOS 17.0, *)
+extension GRPCChannel.Config {
+  init(posix: HTTP2ClientTransport.Config.Posix) {
+    self.init(
+      http2: posix.http2,
+      backoff: posix.backoff,
+      connection: posix.connection,
+      compression: posix.compression
+    )
+  }
+}
---
+++ Sources/GRPCHTTP2TransportNIOPosix/NIOClientBootstrap+SocketAddress.swift
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2024, gRPC Authors All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import GRPCCore
+import GRPCHTTP2Core
+import NIOCore
+import NIOPosix
+
+extension ClientBootstrap {
+  @available(macOS 10.15, iOS 13, tvOS 13, watchOS 6, *)
+  func connect<Result>(
+    to address: GRPCHTTP2Core.SocketAddress,
+    _ configure: @Sendable @escaping (Channel) -> EventLoopFuture<Result>
+  ) async throws -> Result {
+    if let ipv4 = address.ipv4 {
+      return try await self.connect(to: NIOCore.SocketAddress(ipv4), channelInitializer: configure)
+    } else if let ipv6 = address.ipv6 {
+      return try await self.connect(to: NIOCore.SocketAddress(ipv6), channelInitializer: configure)
+    } else if let uds = address.unixDomainSocket {
+      return try await self.connect(to: NIOCore.SocketAddress(uds), channelInitializer: configure)
+    } else if let vsock = address.virtualSocket {
+      return try await self.connect(to: VsockAddress(vsock), channelInitializer: configure)
+    } else {
+      throw RuntimeError(
+        code: .transportError,
+        message: """
+          Unhandled socket address '\(address)', this is a gRPC Swift bug. Please file an issue \
+          against the project.
+          """
+      )
+    }
+  }
+}
+
+extension NIOCore.SocketAddress {
+  init(_ address: GRPCHTTP2Core.SocketAddress.IPv4) throws {
+    try self.init(ipAddress: address.host, port: address.port)
+  }
+
+  init(_ address: GRPCHTTP2Core.SocketAddress.IPv6) throws {
+    try self.init(ipAddress: address.host, port: address.port)
+  }
+
+  init(_ address: GRPCHTTP2Core.SocketAddress.UnixDomainSocket) throws {
+    try self.init(unixDomainSocketPath: address.path)
+  }
+}
+
+extension NIOPosix.VsockAddress {
+  init(_ address: GRPCHTTP2Core.SocketAddress.VirtualSocket) {
+    self.init(
+      cid: ContextID(rawValue: address.contextID.rawValue),
+      port: Port(rawValue: address.port.rawValue)
+    )
+  }
+}
