c83000d464ade65bb6db8d14389a4c32e182d40f
George Barnett <gbarnett@apple.com>
2024-09-02 11:38:02 +0100

    Regenerate protos

--- Tests/GRPCCoreTests/Configuration/Generated/code.pb.swift
+++ Tests/GRPCCoreTests/Configuration/Generated/code.pb.swift
@@ -22,7 +22,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import Foundation
 import SwiftProtobuf
 
 // If the compiler emits an error on this type, it is because this file
