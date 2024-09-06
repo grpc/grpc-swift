ec7045791b8f8f8a11f258265448bcc80f78b6aa
Gus Cairo <gus.cairo@apple.com>
2024-09-06 16:06:14 +0100

    Regenerate

--- Examples/v2/echo/Generated/echo.grpc.swift
+++ Examples/v2/echo/Generated/echo.grpc.swift
@@ -21,8 +21,8 @@
 // For information on using the generated types, please see the documentation:
 //   https://github.com/grpc/grpc-swift
 
-internal import GRPCCore
-internal import GRPCProtobuf
+import GRPCCore
+import GRPCProtobuf
 
 internal enum Echo_Echo {
     internal static let descriptor = GRPCCore.ServiceDescriptor.echo_Echo
