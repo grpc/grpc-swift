9bdb0d1abb36a4601a984af04f4cfaf30862f536
George Barnett <gbarnett@apple.com>
2024-09-20 15:18:57 +0100

    Filter down main to v2 (#2066)
    Motivation:
    
    v2 will be split across a number of packages, and v1 will be maintained
    on the 'release/1.x' branch. In order to do the package split this
    package needs to be whittled down to only the v2 components which will
    remain within this package.
    
    Modifications:
    
    - Remove v1 code
    - Remove v2 code destined for other packages
    - Reshuffle a few scripts
    
    Result:
    
    - Package contains only the intended code

---
+++ dev/check-generated-code.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Copyright 2024, gRPC Authors All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -euo pipefail
+
+log() { printf -- "** %s\n" "$*" >&2; }
+error() { printf -- "** ERROR: %s\n" "$*" >&2; }
+fatal() { error "$@"; exit 1; }
+
+here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Re-generate everything.
+log "Regenerating protos..."
+"$here"/Protos/generate.sh
+
+# Check for changes.
+GIT_PAGER='' git diff --exit-code '*.swift'
+
+log "Generated code is up-to-date"
