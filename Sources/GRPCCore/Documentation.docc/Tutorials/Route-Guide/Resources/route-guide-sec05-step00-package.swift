0696e0adf7e081ba7a757a4c87242a1aab524078
George Barnett <gbarnett@apple.com>
2024-12-20 15:23:23 +0000

    Bump versions (#2150)

--- Sources/GRPCCore/Documentation.docc/Tutorials/Route-Guide/Resources/route-guide-sec05-step00-package.swift
+++ Sources/GRPCCore/Documentation.docc/Tutorials/Route-Guide/Resources/route-guide-sec05-step00-package.swift
@@ -5,8 +5,8 @@ let package = Package(
   name: "RouteGuide",
   platforms: [.macOS(.v15)],
   dependencies: [
-    .package(url: "https://github.com/grpc/grpc-swift.git", from: "2.0.0-beta.1"),
-    .package(url: "https://github.com/grpc/grpc-swift-protobuf.git", from: "1.0.0-beta.1"),
+    .package(url: "https://github.com/grpc/grpc-swift.git", from: "2.0.0-beta.2"),
+    .package(url: "https://github.com/grpc/grpc-swift-protobuf.git", from: "1.0.0-beta.2"),
   ],
   targets: [
     .executableTarget(
